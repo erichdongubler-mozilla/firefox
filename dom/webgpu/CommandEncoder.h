c728c584e2ec35b44ff90d736e22fdb4f1dbfad4
Erich Gubler <erichdongubler@gmail.com>
2023-06-01 13:38:45 -0400

    WIP: Bug ???????: refactor(webgpu): ensure `override` on virtual dtors. r=#webgpu-reviewers

--- dom/webgpu/CommandEncoder.h
+++ dom/webgpu/CommandEncoder.h
@@ -58,7 +58,7 @@ class CommandEncoder final : public ObjectBase, public ChildOf<Device> {
       ffi::WGPUImageCopyTexture_TextureId* aViewFFI);
 
  private:
-  ~CommandEncoder();
+  ~CommandEncoder() override;
   void Cleanup();
 
   RefPtr<WebGPUChild> mBridge;
