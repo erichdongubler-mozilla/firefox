def6a2cde731e4d9532971fb7479cdb40155e4e1
Erich Gubler <erichdongubler@gmail.com>
2023-06-26 14:54:49 -0400

    WIP: asking questions about `gfx_bindings::error::ErrorBuffer`

--- gfx/wgpu_bindings/src/server.rs
+++ gfx/wgpu_bindings/src/server.rs
@@ -2256,6 +2256,8 @@ impl Global {
                     error_buf.init(err, device_id);
                 }
             }
+            // TODO: Why does this even exist? We're round-tripping with only details to burden this
+            // down. Why not just send something to the parent directly?
             DeviceAction::Error { message, r#type } => {
                 error_buf.init(
                     ErrMsg {
