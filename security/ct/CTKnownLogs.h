10e3ee519f426171a85d4fdf4abefde1779b2bce
ffxbld <ffxbld@mozilla.com>
2025-10-16 10:59:45 +0000

    No Bug, mozilla-beta repo-update HSTS HPKP remote-settings ct-logs -  a=pascalc DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D268850

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1766524883000000);
+static const PRTime kCTExpirationTime = INT64_C(1766660084000000);
 
 namespace mozilla::ct {
 
