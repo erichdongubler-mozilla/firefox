526d21a0eb47cfcd4b391895d7f137d8bd32cc16
Adam Vandolder <adam.vandolder@gmail.com>
2025-10-16 20:07:24 +0000

    Bug 1978237 - Check for null before attempting to get document URI when ScriptLoader is reporting to the console. r=dom-core,smaug a=pascalc DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D258259

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -4269,6 +4269,10 @@ void ScriptLoader::ReportErrorToConsole(ScriptLoadRequest* aRequest,
     return;
   }
 
+  if (!mDocument) {
+    return;
+  }
+
   bool isScript = !aRequest->IsModuleRequest();
   const char* message;
   if (aResult == NS_ERROR_MALFORMED_URI) {
@@ -4308,6 +4312,9 @@ void ScriptLoader::ReportErrorToConsole(ScriptLoadRequest* aRequest,
 void ScriptLoader::ReportWarningToConsole(
     ScriptLoadRequest* aRequest, const char* aMessageName,
     const nsTArray<nsString>& aParams) const {
+  if (!mDocument) {
+    return;
+  }
   uint32_t lineNo = aRequest->GetScriptLoadContext()->GetScriptLineNumber();
   JS::ColumnNumberOneOrigin columnNo =
       aRequest->GetScriptLoadContext()->GetScriptColumnNumber();
