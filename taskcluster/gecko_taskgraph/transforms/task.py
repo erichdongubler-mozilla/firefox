3d1afc20a51694e0df6718393e49c8b3122ba474
Ben Hearsum <ben@mozilla.com>
2025-10-27 17:15:59 +0000

    Bug 1972948: remove maxRunTime from scriptworker tasks  a=RyanVM DONTBUILD
    The only scriptworker that supports this is the mac-native notarization poller, which is no longer in use.
    
    Original Revision: https://phabricator.services.mozilla.com/D254321
    
    Differential Revision: https://phabricator.services.mozilla.com/D269600

--- taskcluster/gecko_taskgraph/transforms/task.py
+++ taskcluster/gecko_taskgraph/transforms/task.py
@@ -952,7 +952,6 @@ def build_beetmover_payload(config, task, task_def):
     release_properties = worker["release-properties"]
 
     task_def["payload"] = {
-        "maxRunTime": worker["max-run-time"],
         "releaseProperties": {
             "appName": release_properties["app-name"],
             "appVersion": release_properties["app-version"],
@@ -986,7 +985,6 @@ def build_beetmover_push_to_release_payload(config, task, task_def):
     partners = [f"{p}/{s}" for p, s, _ in get_partners_to_be_published(config)]
 
     task_def["payload"] = {
-        "maxRunTime": worker["max-run-time"],
         "product": worker["product"],
         "version": release_config["version"],
         "build_number": release_config["build_number"],
