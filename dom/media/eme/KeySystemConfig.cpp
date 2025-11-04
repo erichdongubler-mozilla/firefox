890c15b8464a25725c0cf72fbecbef9b4c28f9bd
Cosmin Sabou <csabou@mozilla.com>
2025-11-04 08:58:03 +0200

    Revert "Bug 1900626 - Allow Android to use our bundled clearkey CDM. r=geckoview-reviewers,media-playback-reviewers,jolin,nalexander,tcampbell" for causing wpt failures on encrypted-media-report-only.https.html.
    This reverts commit 1874c449662a06e5a7c5b14f857807db9c543269.

--- dom/media/eme/KeySystemConfig.cpp
+++ dom/media/eme/KeySystemConfig.cpp
@@ -32,15 +32,10 @@ namespace mozilla {
 /* static */
 bool KeySystemConfig::Supports(const nsAString& aKeySystem) {
 #ifdef MOZ_WIDGET_ANDROID
-  // Check if we can use MediaDrm for this keysystem.
+  // No GMP on Android, check if we can use MediaDrm for this keysystem.
   if (mozilla::java::MediaDrmProxy::IsSchemeSupported(
           NS_ConvertUTF16toUTF8(aKeySystem))) {
     return true;
-  }
-  // Check if we can use our bundled Clearkey plugin.
-  if (IsClearkeyKeySystem(aKeySystem)) {
-    return HaveGMPFor(nsCString(CHROMIUM_CDM_API),
-                      {NS_ConvertUTF16toUTF8(aKeySystem)});
   }
 #else
 #  ifdef MOZ_WMF_CDM
--- dom/media/gmp/GMPServiceParent.h
+++ dom/media/gmp/GMPServiceParent.h
@@ -158,8 +158,6 @@ class GeckoMediaPluginServiceParent final
       const nsACString& aAPI, const nsTArray<nsCString>& aTags) override;
 
  private:
-  already_AddRefed<GMPParent> CreateGMPParent();
-
   // Creates a copy of aOriginal. Note that the caller is responsible for
   // adding this to GeckoMediaPluginServiceParent::mPlugins.
   already_AddRefed<GMPParent> ClonePlugin(const GMPParent* aOriginal);
