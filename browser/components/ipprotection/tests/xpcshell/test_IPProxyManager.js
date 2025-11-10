e4049a13c32bfb3f3b22368969721ac216bc6f0b
Andrea Marchesini <amarchesini@mozilla.com>
2025-11-10 14:06:22 +0000

    Bug 1998795 - IPProtection: implement the ACTIVATING state - part 3 - ACTIVE state in the proxy manager, r=ip-protection-reviewers,sstreich
    Differential Revision: https://phabricator.services.mozilla.com/D271668

--- browser/components/ipprotection/tests/xpcshell/test_IPProxyManager.js
+++ browser/components/ipprotection/tests/xpcshell/test_IPProxyManager.js
@@ -4,15 +4,104 @@
 
 "use strict";
 
-const { IPPProxyManager, IPPProxyStates } = ChromeUtils.importESModule(
-  "resource:///modules/ipprotection/IPPProxyManager.sys.mjs"
-);
 const { IPPEnrollAndEntitleManager } = ChromeUtils.importESModule(
   "resource:///modules/ipprotection/IPPEnrollAndEntitleManager.sys.mjs"
 );
 
 add_setup(async function () {
   await putServerInRemoteSettings();
+});
+
+/**
+ * Tests that starting the service gets a state changed event.
+ */
+add_task(async function test_IPPProxyManager_start() {
+  let sandbox = sinon.createSandbox();
+  setupStubs(sandbox);
+
+  IPProtectionService.init();
+
+  await waitForEvent(
+    IPProtectionService,
+    "IPProtectionService:StateChanged",
+    () => IPProtectionService.state === IPProtectionStates.READY
+  );
+
+  Assert.ok(
+    !IPPProxyManager.activatedAt,
+    "IP Protection service should not be active initially"
+  );
+
+  let startedEventPromise = waitForEvent(
+    IPPProxyManager,
+    "IPPProxyManager:StateChanged",
+    () => IPPProxyManager.state === IPPProxyStates.ACTIVE
+  );
+
+  IPPProxyManager.start();
+
+  await startedEventPromise;
+
+  Assert.equal(
+    IPPProxyManager.state,
+    IPPProxyStates.ACTIVE,
+    "IP Protection service should be active after starting"
+  );
+  Assert.ok(
+    !!IPPProxyManager.activatedAt,
+    "IP Protection service should have an activation timestamp"
+  );
+  Assert.ok(
+    IPPProxyManager.active,
+    "IP Protection service should have an active connection"
+  );
+
+  IPProtectionService.uninit();
+  sandbox.restore();
+});
+
+/**
+ * Tests that stopping the service gets stop events.
+ */
+add_task(async function test_IPPProxyManager_stop() {
+  let sandbox = sinon.createSandbox();
+  setupStubs(sandbox);
+
+  const waitForReady = waitForEvent(
+    IPProtectionService,
+    "IPProtectionService:StateChanged",
+    () => IPProtectionService.state === IPProtectionStates.READY
+  );
+
+  IPProtectionService.init();
+  await waitForReady;
+
+  await IPPProxyManager.start();
+
+  let stoppedEventPromise = waitForEvent(
+    IPPProxyManager,
+    "IPPProxyManager:StateChanged",
+    () => IPPProxyManager.state !== IPPProxyStates.ACTIVE
+  );
+  IPPProxyManager.stop();
+
+  await stoppedEventPromise;
+  Assert.notEqual(
+    IPPProxyManager.state,
+    IPPProxyStates.ACTIVE,
+    "IP Protection service should not be active after stopping"
+  );
+  Assert.ok(
+    !IPPProxyManager.activatedAt,
+    "IP Protection service should not have an activation timestamp after stopping"
+  );
+  Assert.ok(
+    !IPProtectionService.connection,
+    "IP Protection service should not have an active connection"
+  );
+
+  IPProtectionService.uninit();
+  sandbox.restore();
 });
 
 /**
@@ -23,13 +112,14 @@ add_task(async function test_IPPProxyManager_start_stop_reset() {
   const sandbox = sinon.createSandbox();
   setupStubs(sandbox);
 
-  IPProtectionService.init();
-
-  await waitForEvent(
+  let readyEvent = waitForEvent(
     IPProtectionService,
     "IPProtectionService:StateChanged",
     () => IPProtectionService.state === IPProtectionStates.READY
   );
+
+  IPProtectionService.init();
+  await readyEvent;
 
   await IPPProxyManager.start();
 
@@ -45,7 +135,7 @@ add_task(async function test_IPPProxyManager_start_stop_reset() {
     "Should have a valid proxy pass after starting"
   );
 
-  await IPPProxyManager.stop();
+  IPPProxyManager.stop();
 
   Assert.ok(!IPPProxyManager.active, "Should not be active after starting");
 
@@ -129,12 +219,77 @@ add_task(async function test_IPPProxyStates_error() {
     "IP Protection service should be ready"
   );
 
-  await IPProtectionService.start(false);
+  await IPPProxyManager.start(false);
 
   Assert.equal(
     IPPProxyManager.state,
     IPPProxyStates.ERROR,
     "IP Protection service should be active"
+  );
+
+  IPProtectionService.uninit();
+  sandbox.restore();
+});
+
+/**
+ * Tests the active state.
+ */
+add_task(async function test_IPPProxytates_active() {
+  let sandbox = sinon.createSandbox();
+  sandbox.stub(IPPSignInWatcher, "isSignedIn").get(() => true);
+  sandbox
+    .stub(IPProtectionService.guardian, "isLinkedToGuardian")
+    .resolves(true);
+  sandbox.stub(IPProtectionService.guardian, "fetchUserInfo").resolves({
+    status: 200,
+    error: undefined,
+    entitlement: { uid: 42 },
+  });
+  sandbox.stub(IPProtectionService.guardian, "fetchProxyPass").resolves({
+    status: 200,
+    error: undefined,
+    pass: {
+      isValid: () => options.validProxyPass,
+      asBearerToken: () => "Bearer helloworld",
+    },
+  });
+
+  const waitForReady = waitForEvent(
+    IPProtectionService,
+    "IPProtectionService:StateChanged",
+    () => IPProtectionService.state === IPProtectionStates.READY
+  );
+
+  IPProtectionService.init();
+
+  await waitForReady;
+
+  Assert.equal(
+    IPProtectionService.state,
+    IPProtectionStates.READY,
+    "IP Protection service should be ready"
+  );
+
+  await IPPProxyManager.start(false);
+
+  Assert.equal(
+    IPProtectionService.state,
+    IPProtectionStates.READY,
+    "IP Protection service should be in ready state"
+  );
+
+  Assert.equal(
+    IPPProxyManager.state,
+    IPPProxyStates.ACTIVE,
+    "IP Protection service should be active"
+  );
+
+  await IPPProxyManager.stop(false);
+
+  Assert.equal(
+    IPProtectionService.state,
+    IPProtectionStates.READY,
+    "IP Protection service should be ready again"
   );
 
   IPProtectionService.uninit();
