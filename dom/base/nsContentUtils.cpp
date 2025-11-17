46e52c831cb2aff47a901d296c519b12848fca8d
Emilio Cobos Álvarez <emilio@crisal.io>
2025-11-17 16:29:06 +0900

    Bug 2000153 - Don't lose track of an event status we care about.

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -9630,7 +9630,7 @@ Result<bool, nsresult> nsContentUtils::SynthesizeMouseEvent(
              StaticPrefs::test_events_async_enabled()) {
     status = aWidget->DispatchInputEvent(&mouseOrPointerEvent).mContentStatus;
   } else {
-    aWidget->DispatchEvent(&mouseOrPointerEvent);
+    status = aWidget->DispatchEvent(&mouseOrPointerEvent);
   }
 
   // The callback ID may be cleared when the event also needs to be dispatched
