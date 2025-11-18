a1aad2e716e9ba326bfeab248c59f920ce8887e5
kpatenio <kpatenio@mozilla.com>
2025-11-18 22:19:54 +0000

    Bug 2000728 — add pref to show/hide ipp exceptions in about:preferences. r=ip-protection-reviewers,mconley,fchasen
    Differential Revision: https://phabricator.services.mozilla.com/D272968

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3517,6 +3517,8 @@ pref("browser.ipProtection.userEnabled", false);
 pref("browser.ipProtection.variant", "");
 // Pref to track number of times the VPN panel is opened
 pref("browser.ipProtection.panelOpenCount", 0);
+// Pref to enable support for site exceptions
+pref("browser.ipProtection.features.siteExceptions", false);
 pref("browser.ipProtection.exceptionsMode", "all");
 pref("browser.ipProtection.domainExclusions", "");
 pref("browser.ipProtection.domainInclusions", "");
