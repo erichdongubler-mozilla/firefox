bc732e77b57e7512a01f80664eb18f553633982b
Nazım Can Altınova <canaltinova@gmail.com>
2025-11-19 14:36:01 +0000

    Bug 2000426 - Convert EventTiming mDuration to an optional value r=smaug
    This patch converts the mDuration of PerformanceEventTiming from
    DOMHighResTimeStamp to Maybe<DOMHighResTimeStamp>. This allows us to
    distinguish between:
    - Duration not yet set (mDuration.isNothing())
    - Duration explicitly set to 0 (mDuration.isSome() && value == 0)
    
    This distinction is necessary to fix bug 2000426, where we need to
    determine if a pointerdown event's duration has been finalized before
    processing subsequent pointerup/click events.
    
    Differential Revision: https://phabricator.services.mozilla.com/D273025

--- dom/performance/PerformanceEventTiming.h
+++ dom/performance/PerformanceEventTiming.h
@@ -75,18 +75,18 @@ class PerformanceEventTiming final
   void SetDuration(const DOMHighResTimeStamp aDuration) {
     // Round the duration to the nearest 8ms.
     // https://w3c.github.io/event-timing/#set-event-timing-entry-duration
-    mDuration = std::round(aDuration / 8) * 8;
+    mDuration = Some(std::round(aDuration / 8) * 8);
   }
 
   // nsRFPService::ReduceTimePrecisionAsMSecs might causes
   // some memory overhead, using the raw timestamp internally
   // to avoid calling in unnecessarily.
-  DOMHighResTimeStamp RawDuration() const { return mDuration; }
+  Maybe<DOMHighResTimeStamp> RawDuration() const { return mDuration; }
 
   DOMHighResTimeStamp Duration() const override {
     if (mCachedDuration.isNothing()) {
       mCachedDuration.emplace(nsRFPService::ReduceTimePrecisionAsMSecs(
-          mDuration, mPerformance->GetRandomTimelineSeed(),
+          mDuration.valueOr(0), mPerformance->GetRandomTimelineSeed(),
           mPerformance->GetRTPCallerType()));
     }
     return mCachedDuration.value();
@@ -136,7 +136,7 @@ class PerformanceEventTiming final
   DOMHighResTimeStamp mStartTime;
   mutable Maybe<DOMHighResTimeStamp> mCachedStartTime;
 
-  DOMHighResTimeStamp mDuration;
+  Maybe<DOMHighResTimeStamp> mDuration;
   mutable Maybe<DOMHighResTimeStamp> mCachedDuration;
 
   bool mCancelable;
