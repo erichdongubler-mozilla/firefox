63a23a03e0a36855c9a2a144a6379772de36ea01
Eitan Isaacson <eitan@monotonous.org>
2025-11-20 20:05:22 +0000

    Bug 1998242 - P3: Introduce DirectName() to be used by Name(). r=morgan
    Mark Name() as final in LocalAccessible and RemoteAccessible, and rely
    on DirectName for overriding name calculation in local accessibles.
    
    Depends on D271310
    
    Differential Revision: https://phabricator.services.mozilla.com/D271311

--- accessible/html/HTMLFormControlAccessible.cpp
+++ accessible/html/HTMLFormControlAccessible.cpp
@@ -331,8 +331,8 @@ already_AddRefed<AccAttributes> HTMLTextFieldAccessible::NativeAttributes() {
   return attributes.forget();
 }
 
-ENameValueFlag HTMLTextFieldAccessible::Name(nsString& aName) const {
-  ENameValueFlag nameFlag = LocalAccessible::Name(aName);
+ENameValueFlag HTMLTextFieldAccessible::DirectName(nsString& aName) const {
+  ENameValueFlag nameFlag = LocalAccessible::DirectName(aName);
   if (!aName.IsEmpty()) return nameFlag;
 
   // text inputs and textareas might have useful placeholder text
@@ -510,8 +510,8 @@ bool HTMLFileInputAccessible::IsAcceptableChild(nsIContent* aEl) const {
   return aEl->IsText();
 }
 
-ENameValueFlag HTMLFileInputAccessible::Name(nsString& aName) const {
-  ENameValueFlag flag = HyperTextAccessible::Name(aName);
+ENameValueFlag HTMLFileInputAccessible::DirectName(nsString& aName) const {
+  ENameValueFlag flag = HyperTextAccessible::DirectName(aName);
   if (flag == eNameFromSubtree) {
     // The author didn't provide a name. We'll compute the name from our subtree
     // below.
