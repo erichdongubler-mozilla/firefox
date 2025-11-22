b20c4113043b8a50ba7743d922ef48863cfcc7a9
Gregory Pappas <gp3033@protonmail.com>
2025-11-22 18:44:13 +0000

    Bug 2001783 - Expose gfx.compositor.gpu-migration pref to macOS only r=gfx-reviewers,lsalzman
    Also simplify. In the past, gpu migration was enabled for Layers but disabled
    for WebRender and could be force enabled with the third state of this pref.
    Now there is only WebRender!
    
    Differential Revision: https://phabricator.services.mozilla.com/D273711

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -1170,7 +1170,7 @@ bool nsCocoaWindow::PreRender(WidgetRenderingContext* aContext)
   // composition is done, thus keeping the GL context locked forever.
   mCompositingLock.Lock();
 
-  if (aContext->mGL && gfxPlatform::CanMigrateMacGPUs()) {
+  if (aContext->mGL && StaticPrefs::gfx_compositor_gpu_migration()) {
     GLContextCGL::Cast(aContext->mGL)->MigrateToActiveGPU();
   }
 
