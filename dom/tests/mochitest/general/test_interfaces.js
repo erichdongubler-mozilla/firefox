42c61dc9b52d47cf151f252282c36bc545be3921
Alexandru Marc <amarc@mozilla.com>
2025-11-26 08:29:17 +0200

    Revert "Bug 2001878 - Implement :active-view-transition-type and co. r=firefox-style-system-reviewers,view-transitions-reviewers,webidl,smaug,boris" for causing wpt failures @ view-transition-types-mutable-no-document-element-crashtest.html
    This reverts commit b7f6d2d170499d5c2dc8f27092c8a1741e0212fa.

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -1609,8 +1609,6 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "ViewTransition", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "ViewTransitionTypeSet", insecureContext: true },
-  // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "VisualViewport", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "WGSLLanguageFeatures", earlyBetaOrEarlier: true },
