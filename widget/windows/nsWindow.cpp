b7eaadfba3f7241dc3b85cf11c05bbd7791648ff
Emilio Cobos Álvarez <emilio@crisal.io>
2025-11-27 16:34:03 +0100

    Bug 1993474 - Remove redundant nsWindow::mPIPWindow. r=#win-reviewers a=pascalc
    nsIWidget already has a member for this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D274282

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -818,7 +818,6 @@ static bool IsCloaked(HWND hwnd) {
 nsWindow::nsWindow()
     : nsIWidget(BorderStyle::Default),
       mFrameState(std::in_place, this),
-      mPIPWindow(false),
       mMicaBackdrop(false),
       mLastPaintEndTime(TimeStamp::Now()),
       mCachedHitTestTime(TimeStamp::Now()),
@@ -1030,7 +1029,6 @@ nsresult nsWindow::Create(nsIWidget* aParent, const LayoutDeviceIntRect& aRect,
       aParent ? (HWND)aParent->GetNativeData(NS_NATIVE_WINDOW) : nullptr;
 
   mIsRTL = aInitData.mRTL;
-  mPIPWindow = aInitData.mPIPWindow;
   mOpeningAnimationSuppressed = aInitData.mIsAnimationSuppressed;
   mAlwaysOnTop = aInitData.mAlwaysOnTop;
   mIsAlert = aInitData.mIsAlert;
@@ -2809,7 +2807,7 @@ bool nsWindow::UpdateNonClientMargins(bool aReflowWindow) {
     // frame sizes for left, right and bottom since Windows will automagically
     // position the edges "offscreen" for maximized windows.
     metrics.mOffset.top = metrics.mCaptionHeight;
-  } else if (mPIPWindow &&
+  } else if (mIsPIPWindow &&
              !StaticPrefs::widget_windows_pip_decorations_enabled()) {
     metrics.mOffset = metrics.DefaultMargins();
   } else {
