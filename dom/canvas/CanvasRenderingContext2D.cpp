a66fab0931d975204ebe2cf943bf6885c756acf1
Tom Ritter <tom@mozilla.com>
2025-12-08 16:31:01 +0000

    Bug 1873716: Switch to using a bitmask for known text r=timhuang
    SKIP_BMO_CHECK
    
    Differential Revision: https://phabricator.services.mozilla.com/D273662

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -135,6 +135,8 @@ using namespace mozilla::image;
 using namespace mozilla::ipc;
 using namespace mozilla::layers;
 
+static mozilla::LazyLogModule gFingerprinterDetection("FingerprinterDetection");
+
 namespace mozilla::dom {
 
 // Cap sigma to avoid overly large temp surfaces.
@@ -4566,25 +4568,65 @@ void CanvasRenderingContext2D::FillText(const nsAString& aText, double aX,
                                         const Optional<double>& aMaxWidth,
                                         ErrorResult& aError) {
   // We try to match the most commonly observed strings used by canvas
-  // fingerprinting scripts. We do a prefix match, because that means having to
-  // match fewer bytes and sometimes the strings is followed by a few random
-  // characters.
-  // - Cwm fjordbank gly
-  //   Used by FingerprintJS
-  //   (https://github.com/fingerprintjs/fingerprintjs/blob/4c4b2c8455e701b8341b2b766d1939cf5de4b615/src/sources/canvas.ts#L119)
-  //   and others
-  // - Hel$&?6%){mZ+#@
-  // - <@nv45. F1n63r,Pr1n71n6!
-  // Usually there are at most a handful (usually ~1/2) fillText calls by
-  // fingerprinters
+  // fingerprinting scripts.
+  MOZ_LOG(gFingerprinterDetection, LogLevel::Verbose,
+          ("mFillTextCalls %i FillText: "
+           "\"%s\"\n",
+           mFillTextCalls, NS_ConvertUTF16toUTF8(aText).get()));
   if (mFillTextCalls <= 5) {
-    if (StringBeginsWith(aText, u"Cwm fjord"_ns) ||
-        StringBeginsWith(aText, u"Hel$&?6%"_ns) ||
-        StringBeginsWith(aText, u"<@nv45. "_ns)) {
-      mFeatureUsage |= CanvasFeatureUsage::KnownFingerprintText;
+    if (aText == u"Cwm fjordbank glyphs vext quiz, 😃"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_1;
+    } else if (aText == u"Cwm fjordbank gly 😃"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_1;
+    } else if (StringBeginsWith(aText, u"Hel$&?6%"_ns)) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_2;
+    } else if (StringBeginsWith(aText, u"<@nv45. "_ns)) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_3;
+    } else if (aText == u"Cañvas FP 😎 12345"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_4;
+    } else if (StringBeginsWith(aText, u"❤️🤪🎉👋"_ns)) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_5;
+    } else if (aText == u"SomeCanvasFingerPrint.65@345876"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_6;
+    } else if (aText == u"Browser,Signal <canvas> 2.0"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_7;
+    } else if (aText == u"@Browsers~%fingGPRint$&,<canvas>"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_8;
+    } else if (aText == u"M"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_9;
+    } else if (aText == u"E"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_10;
+    } else if (aText == u"g"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_11;
+    } else if (aText == u"Soft Ruddy Foothold 2"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_12;
+    } else if (aText == u"!H71JCaj)]# 1@#"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_13;
+    } else if (aText == u"oubrg5h56e@!$3t4"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_14;
+    } else if (aText == u"Cwm fjordbank glyphs vext quiz,"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_15;
+    } else if (aText == u"ClientJS,org <canvas> 1.0"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_16;
+    } else if (aText == u"IaID,org <canvas> 1.0"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_17;
+    } else if (aText == u"conviva"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_18;
+    } else if (aText == u"Random Text WMwmil10Oo"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_19;
+    } else if (aText == u"-0.5753861119575491"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_20;
+    } else if (aText == u"0.8178819121159085"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_21;
+    } else if (StringBeginsWith(aText, u"Cwm fjordbank"_ns)) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_22;
+    } else if (StringBeginsWith(aText, u"iO0A"_ns)) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_23;
+    } else if (aText == u"<@nv45. F1n63r,Pr1n71n6!"_ns) {
+      mFeatureUsage |= CanvasFeatureUsage::KnownText_24;
     }
-    mFillTextCalls++;
   }
+  mFillTextCalls++;
 
   DebugOnly<UniquePtr<TextMetrics>> metrics = DrawOrMeasureText(
       aText, aX, aY, aMaxWidth, TextDrawOperation::FILL, aError);
