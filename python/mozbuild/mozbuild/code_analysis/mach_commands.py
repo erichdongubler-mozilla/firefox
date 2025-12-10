fa8e7abb4ad360508a419ae5511d237694dcecff
Alex Hochheiden <ahochheiden@mozilla.com>
2025-12-10 06:03:25 +0000

    Bug 2003412 - Fix PLW0127 warnings: Remove self-assigning variable r=emilio
    https://docs.astral.sh/ruff/rules/self-assigning-variable/
    
    Differential Revision: https://phabricator.services.mozilla.com/D274698

--- python/mozbuild/mozbuild/code_analysis/mach_commands.py
+++ python/mozbuild/mozbuild/code_analysis/mach_commands.py
@@ -619,7 +619,6 @@ def autotest(
     # do this on a local trusted clang-tidy package.
     command_context._set_log_level(verbose)
     command_context.activate_virtualenv()
-    dump_results = dump_results
 
     force_download = not dump_results
 
