eddf4c14825c22ce93798d9d5bad09842c0aa56e
Edgar Chen <echen@mozilla.com>
2025-12-11 12:12:32 +0000

    Bug 2005081 - Ensure clipboard paste button is dismissed after navigation; r=Gijs,tschuster
    Differential Revision: https://phabricator.services.mozilla.com/D275694

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2250,7 +2250,7 @@ var XULBrowserWindow = {
 
     // Ensure we close any remaining open locationspecific panels
     if (!isSameDocument) {
-      closeOpenPanels("panel[locationspecific='true']");
+      closeOpenPanels(":is(panel, menupopup)[locationspecific='true']");
     }
 
     gPermissionPanel.onLocationChange();
