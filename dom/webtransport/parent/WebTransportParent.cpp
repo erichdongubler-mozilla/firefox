78df9f05123e2639ff5059c3e1b07de81d23c2b9
Hubert Boma Manilla <hmanilla@mozilla.com>
2025-12-15 13:28:02 +0000

    Bug 1999240 - Add API's for monitoring Webtransport sessions creations and destruction r=necko-reviewers,kershaw,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D265607

--- dom/webtransport/parent/WebTransportParent.cpp
+++ dom/webtransport/parent/WebTransportParent.cpp
@@ -725,6 +725,16 @@ WebTransportParent::OnIncomingBidirectionalStreamAvailable(
   return IPC_OK();
 }
 
+::mozilla::ipc::IPCResult WebTransportParent::RecvGetHttpChannelID(
+    GetHttpChannelIDResolver&& aResolver) {
+  LOG(("WebTransportParent Channel ID"));
+  MOZ_ASSERT(mWebTransport);
+  uint64_t aHttpChannelId;
+  mWebTransport->GetHttpChannelID(&aHttpChannelId);
+  aResolver(aHttpChannelId);
+  return IPC_OK();
+}
+
 // The promise sent in this request will be resolved
 // in OnOutgoingDatagramOutCome which is called synchronously from
 // WebTransportSessionProxy::SendDatagram
