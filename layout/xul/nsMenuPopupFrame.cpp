f465bef00d657c5295a5e309a5ae38886bdd1272
Emilio Cobos Álvarez <emilio@crisal.io>
2025-12-16 22:25:16 +0000

    Bug 2006320 - Clean up WindowMoved / WindowResized to use typed units. r=win-reviewers,gstoll
    Make sure that we notify of outer position and inner sizes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D276645

--- layout/xul/nsMenuPopupFrame.cpp
+++ layout/xul/nsMenuPopupFrame.cpp
@@ -2505,22 +2505,21 @@ void nsMenuPopupFrame::CheckForAnchorChange(nsRect& aRect) {
   }
 }
 
-bool nsMenuPopupFrame::WindowMoved(nsIWidget* aWidget, int32_t aX, int32_t aY,
+void nsMenuPopupFrame::WindowMoved(nsIWidget* aWidget,
+                                   const LayoutDeviceIntPoint& aPoint,
                                    ByMoveToRect aByMoveToRect) {
   MOZ_ASSERT(aWidget == mWidget);
 
   if (!IsVisibleOrShowing()) {
-    return true;
+    return;
   }
-
-  LayoutDeviceIntPoint point(aX, aY);
 
   // Don't do anything if the popup is already at the specified location. This
   // prevents recursive calls when a popup is positioned.
   LayoutDeviceIntRect curDevBounds = CalcWidgetBounds();
-  if (curDevBounds.TopLeft() == point &&
+  if (curDevBounds.TopLeft() == aPoint &&
       aWidget->GetClientOffset() == GetLastClientOffset()) {
-    return true;
+    return;
   }
 
   // Update the popup's position using SetPopupPosition if the popup is
@@ -2532,46 +2531,43 @@ bool nsMenuPopupFrame::WindowMoved(nsIWidget* aWidget, int32_t aX, int32_t aY,
       aByMoveToRect == ByMoveToRect::No) {
     SetPopupPosition(true);
   } else {
-    CSSPoint cssPos = point / PresContext()->CSSToDevPixelScale();
+    CSSPoint cssPos = aPoint / PresContext()->CSSToDevPixelScale();
     MoveTo(cssPos, false, aByMoveToRect == ByMoveToRect::Yes);
   }
-  return true;
 }
 
-bool nsMenuPopupFrame::WindowResized(nsIWidget* aWidget, int32_t aWidth,
-                                     int32_t aHeight) {
+void nsMenuPopupFrame::WindowResized(nsIWidget* aWidget,
+                                     const LayoutDeviceIntSize& aSize) {
   MOZ_ASSERT(aWidget == mWidget);
   if (!IsVisibleOrShowing()) {
-    return true;
+    return;
   }
 
-  LayoutDeviceIntSize size(aWidth, aHeight);
   const LayoutDeviceIntRect curDevBounds = CalcWidgetBounds();
   // If the size is what we think it is, we have nothing to do.
-  if (curDevBounds.Size() == size) {
-    return true;
+  if (curDevBounds.Size() == aSize) {
+    return;
   }
 
   RefPtr<Element> popup = &PopupElement();
 
   // Only set the width and height if the popup already has these attributes.
   if (!popup->HasAttr(nsGkAtoms::width) || !popup->HasAttr(nsGkAtoms::height)) {
-    return true;
+    return;
   }
 
   // The size is different. Convert the actual size to css pixels and store it
   // as 'width' and 'height' attributes on the popup.
   nsPresContext* presContext = PresContext();
 
-  CSSIntSize newCSS(presContext->DevPixelsToIntCSSPixels(size.width),
-                    presContext->DevPixelsToIntCSSPixels(size.height));
+  CSSIntSize newCSS(presContext->DevPixelsToIntCSSPixels(aSize.width),
+                    presContext->DevPixelsToIntCSSPixels(aSize.height));
 
   nsAutoString width, height;
   width.AppendInt(newCSS.width);
   height.AppendInt(newCSS.height);
   popup->SetAttr(kNameSpaceID_None, nsGkAtoms::width, width, true);
   popup->SetAttr(kNameSpaceID_None, nsGkAtoms::height, height, true);
-  return true;
 }
 
 bool nsMenuPopupFrame::RequestWindowClose(nsIWidget* aWidget) {
