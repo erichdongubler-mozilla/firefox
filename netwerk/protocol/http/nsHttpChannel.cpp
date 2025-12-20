4fd058453e5668dd6a39a8e6bf92e58fd11c6490
Serban Stanca <sstanca@mozilla.com>
2025-12-20 05:01:36 +0200

    Revert "Bug 2005990 - Skip LNA checks when captive portal is active. r=necko-reviewers,valentin" for causing mochitests failures in browser_captivePortal_lna.js.
    This reverts commit d5e7c19b494ed3306fb4e745597472f05165a663.
    
    This reverts commit 9e69bd91cac88025a86f851be3eba8759d8a1a14.
    
    This reverts commit 233b190d8229822d9532e2de6dae444527d4dd64.

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -17,7 +17,6 @@
 #include "mozilla/glean/AntitrackingMetrics.h"
 #include "mozilla/glean/NetwerkMetrics.h"
 #include "mozilla/glean/NetwerkProtocolHttpMetrics.h"
-#include "mozilla/net/CaptivePortalService.h"
 #include "mozilla/net/CookieServiceParent.h"
 #include "mozilla/StoragePrincipalHelper.h"
 
@@ -2086,28 +2085,6 @@ LNAPermission nsHttpChannel::UpdateLocalNetworkAccessPermissions(
   }
 
   MOZ_ASSERT(mLoadInfo->TriggeringPrincipal(), "need triggering principal");
-
-  // Skip LNA checks if the triggering principal and target are same origin
-  // Note: This could be a case where there is a network change or device
-  // migration to a private or corporate network
-  bool isSameOrigin = false;
-  nsresult rv =
-      mLoadInfo->TriggeringPrincipal()->IsSameOrigin(mURI, &isSameOrigin);
-  if (NS_SUCCEEDED(rv) && isSameOrigin) {
-    userPerms = LNAPermission::Granted;
-    return userPerms;
-  }
-
-  // Skip LNA checks if captive portal is active
-  nsCOMPtr<nsICaptivePortalService> cps = CaptivePortalService::GetSingleton();
-  if (cps) {
-    int32_t state = cps->State();
-    if (state == nsICaptivePortalService::LOCKED_PORTAL &&
-        aPermissionType == LOCAL_NETWORK_PERMISSION_KEY) {
-      userPerms = LNAPermission::Granted;
-      return userPerms;
-    }
-  }
 
   // Step 1. Check for  Existing Allow or Deny permission
   if (nsContentUtils::IsExactSitePermAllow(mLoadInfo->TriggeringPrincipal(),
