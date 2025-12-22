0895ef714d19de7b35c74614283fc6a8311d0c7c
Mark Banner <standard8@mozilla.com>
2025-12-22 12:39:07 +0000

    Bug 2004680 - Fix ESLint rule jsdoc/check-tag-names issues in dom. r=dom-core,media-playback-reviewers,smaug,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D275757

--- dom/xhr/tests/test_XHR_timeout.js
+++ dom/xhr/tests/test_XHR_timeout.js
@@ -50,10 +50,10 @@ function ok(bool, msg) {
  *                            reset the timeout.
  * @param {number} resetTo    (Optional) The delay to reset the timeout to.
  *
- * @note The actual testing takes place in handleEvent(event).
+ * Note: The actual testing takes place in handleEvent(event).
  * The requests are generated in startXHR().
  *
- * @note If resetAfter and resetTo are omitted, only the initial timeout setting
+ * Note: If resetAfter and resetTo are omitted, only the initial timeout setting
  * applies.
  *
  * @class
