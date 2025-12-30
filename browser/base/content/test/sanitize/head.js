1959300e0cb4fccc0ff13df3a5f4abeba4db98db
Cristina Horotan <chorotan@mozilla.com>
2025-12-30 17:48:45 +0200

    Revert "Bug 2001196, Bug 2001195, Bug 2001194 - Implement "History" card - r=emz,fluent-reviewers,akulyk,bolsson" for causing ESlint failure
    This reverts commit cfbe87ee22857758128bf11690eb33f7c6662a18.
    
    Revert "Bug 2001195 - Implement Browsing data card - r=emz,fluent-reviewers,akulyk,bolsson"
    
    This reverts commit a0ded69d79fba8f16a406ebc35c7eb2e78123019.
    
    Revert "Bug 2001194 - Implement "Additional Protections" card - r=emz,fluent-reviewers,credential-management-reviewers,akulyk,bolsson,mtigley"
    
    This reverts commit 11cc2fb5d010f66c02da1badacf25ed745efb9cf.

--- browser/base/content/test/sanitize/head.js
+++ browser/base/content/test/sanitize/head.js
@@ -499,15 +499,6 @@ function promiseSanitizationComplete() {
   return TestUtils.topicObserved("sanitizer-sanitization-complete");
 }
 
-function settingsRedesignHistoryEnabled() {
-  return (
-    Services.prefs.getBoolPref(
-      "browser.settings-redesign.history2.enabled",
-      false
-    ) || Services.prefs.getBoolPref("browser.settings-redesign.enabled", false)
-  );
-}
-
 /**
  * This wraps the dialog and provides some convenience methods for interacting
  * with it.
@@ -660,15 +651,9 @@ ClearHistoryDialogHelper.prototype = {
     // We want to simulate opening the dialog inside preferences for clear history
     // and clear site data
     if (this._mode != "browser") {
-      if (this._mode == "clearOnShutdown" && settingsRedesignHistoryEnabled()) {
-        await openPreferencesViaOpenPreferencesAPI("history", {
-          leaveOpen: true,
-        });
-      } else {
-        await openPreferencesViaOpenPreferencesAPI("privacy", {
-          leaveOpen: true,
-        });
-      }
+      await openPreferencesViaOpenPreferencesAPI("privacy", {
+        leaveOpen: true,
+      });
       let tabWindow = gBrowser.selectedBrowser.contentWindow;
       let clearDialogOpenButtonId = this._mode + "Button";
       // the id for clear on shutdown is of a different format
--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { HeadlessShell } from "../shell/HeadlessShell.sys.mjs";
-
 /* import-globals-from extensionControlled.js */
 /* import-globals-from preferences.js */
 
@@ -204,11 +202,9 @@ Preferences.addAll([
   // Do not track and Global Privacy Control
   { id: "privacy.donottrackheader.enabled", type: "bool" },
   { id: "privacy.globalprivacycontrol.functionality.enabled", type: "bool" },
+
+  // Global Privacy Control
   { id: "privacy.globalprivacycontrol.enabled", type: "bool" },
-  {
-    id: "browser.preferences.config_warning.donottrackheader.dismissed",
-    type: "bool",
-  },
 
   // Firefox VPN
   { id: "browser.ipProtection.enabled", type: "bool" },
@@ -1532,56 +1528,14 @@ Preferences.addSetting({
   },
 });
 Preferences.addSetting({
-  id: "relayFeature",
-  pref: "signon.firefoxRelay.feature",
-});
-Preferences.addSetting({
-  id: "relayIntegration",
-  deps: ["savePasswords", "relayFeature"],
-  visible: () => {
-    return FirefoxRelay.isAvailable;
-  },
-  disabled: ({ savePasswords, relayFeature }) => {
-    return !savePasswords.value || relayFeature.pref.locked;
-  },
-  get() {
-    return FirefoxRelay.isAvailable && !FirefoxRelay.isDisabled;
-  },
-  set(checked) {
-    if (checked) {
-      FirefoxRelay.markAsAvailable();
-    } else {
-      FirefoxRelay.markAsDisabled();
-    }
-  },
-  onUserChange(checked) {
-    if (checked) {
-      Glean.relayIntegration.enabledPrefChange.record();
-    } else {
-      Glean.relayIntegration.disabledPrefChange.record();
-    }
-  },
-});
-Preferences.addSetting({
   id: "dntHeaderEnabled",
   pref: "privacy.donottrackheader.enabled",
 });
 Preferences.addSetting({
   id: "dntRemoval",
-  pref: "browser.preferences.config_warning.donottrackheader.dismissed",
   deps: ["dntHeaderEnabled"],
-  visible: ({ dntHeaderEnabled }, setting) => {
-    return dntHeaderEnabled.value && !setting.value;
-  },
-  onUserClick: (event, _deps, setting) => {
-    let dismissButton = event.target?.shadowRoot?.querySelector(".close");
-    if (
-      dismissButton?.shadowRoot &&
-      event.originalTarget &&
-      dismissButton.shadowRoot.contains(event.originalTarget)
-    ) {
-      setting.value = true;
-    }
+  visible: ({ dntHeaderEnabled }) => {
+    return dntHeaderEnabled.value;
   },
 });
 
@@ -2178,15 +2132,13 @@ Preferences.addSetting({
     return privateBrowsingAutoStart.locked && privateBrowsingAutoStart.value;
   },
   getControlConfig(config, { privateBrowsingAutoStart }, setting) {
-    let l10nId = undefined;
-    if (!srdSectionEnabled("history2")) {
-      if (setting.value == "remember") {
-        l10nId = "history-remember-description3";
-      } else if (setting.value == "dontremember") {
-        l10nId = "history-dontremember-description3";
-      } else if (setting.value == "custom") {
-        l10nId = "history-custom-description3";
-      }
+    let l10nId = null;
+    if (setting.value == "remember") {
+      l10nId = "history-remember-description3";
+    } else if (setting.value == "dontremember") {
+      l10nId = "history-dontremember-description3";
+    } else if (setting.value == "custom") {
+      l10nId = "history-custom-description3";
     }
 
     let dontRememberOption = config.options.find(
@@ -2205,14 +2157,6 @@ Preferences.addSetting({
       ...config,
       l10nId,
     };
-  },
-});
-
-Preferences.addSetting({
-  id: "customHistoryButton",
-  onUserClick(e) {
-    e.preventDefault();
-    gotoPref("paneHistory");
   },
 });
 
@@ -3783,17 +3727,14 @@ var gPrivacyPane = {
    */
   init() {
     initSettingGroup("nonTechnicalPrivacy");
-    initSettingGroup("nonTechnicalPrivacy2");
     initSettingGroup("securityPrivacyStatus");
     initSettingGroup("securityPrivacyWarnings");
     initSettingGroup("httpsOnly");
     initSettingGroup("browsingProtection");
     initSettingGroup("cookiesAndSiteData");
-    initSettingGroup("cookiesAndSiteData2");
     initSettingGroup("certificates");
     initSettingGroup("ipprotection");
     initSettingGroup("history");
-    initSettingGroup("history2");
     initSettingGroup("permissions");
     initSettingGroup("dnsOverHttps");
     initSettingGroup("dnsOverHttpsAdvanced");
