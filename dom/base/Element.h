33130e712d0acf4a43cc8627d188112c3806dbb7
Alice Boxhall <95208+alice@users.noreply.github.com>
2026-01-06 16:13:55 +0000

    Bug 1984685 - Refactor: consistently return Maybe<nsTArray<RefPtr<Element>>> when getting attr-associated elements, and key AttrRelProviders on nsAtom rather than nsString. r=dom-core,morgan,Jamie,smaug
    The existing `Element::GetAttrAssociatedElements()` and its internal lambda (which the subsequent change promotes into the `GetAttrAssociatedElementsInternal()` method, in order to use it as the internal "source of truth" method) already used arrays of `RefPtrs`, since the former interacts with scripting. In the subsequent change, since the `GetAttrAssociatedElementsInternal()` method which I adapted from the original lambda is used both by the bindings-exposed `GetAttrAssociatedElementsForBindings()` and in `AssociatedElementsIterator()`, I thought it was more straightforward to adapt `AssociatedElementsIterator` to use the array of `RefPtrs` rather than unpack the raw pointers each time, but I'm happy to modify it if this will be too slow or memory-intensive.
    
    Differential Revision: https://phabricator.services.mozilla.com/D262170

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -1406,8 +1406,8 @@ class Element : public FragmentOrElement {
    * shadow-including ancestors. It also does not attempt to retrieve elements
    * using the ids set in the content attribute.
    */
-  void GetExplicitlySetAttrElements(nsAtom* aAttr,
-                                    nsTArray<Element*>& aElements) const;
+  Maybe<nsTArray<RefPtr<dom::Element>>> GetExplicitlySetAttrElements(
+      nsAtom* aAttr) const;
 
   PseudoStyleType GetPseudoElementType() const {
     nsresult rv = NS_OK;
