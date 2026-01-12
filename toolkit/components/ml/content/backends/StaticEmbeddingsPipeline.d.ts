3067879bd041ebd5c746be13516e05a8630ca83b
Greg Tatum <tatum.creative@gmail.com>
2026-01-12 14:27:58 +0000

    Bug 1998228 - Add toolkit/components/ml/tsconfig.json for some initial type checking r=ai-frontend-reviewers,Standard8,ai-ondevice-reviewers,tarek
    Differential Revision: https://phabricator.services.mozilla.com/D271261

--- toolkit/components/ml/content/backends/StaticEmbeddingsPipeline.d.ts
+++ toolkit/components/ml/content/backends/StaticEmbeddingsPipeline.d.ts
@@ -89,6 +89,6 @@ export interface StaticEmbeddingsOptions {
  * @see https://huggingface.co/docs/transformers.js/api/tokenizers#module_tokenizers.PreTrainedTokenizer
  */
 interface PreTrainedTokenizer {
-  model: { vocab: list[any] };
+  model: { vocab: Array<any> };
   encode(text: string): number[];
 }
