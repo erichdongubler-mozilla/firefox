8b54a4cb70c8b6d5a24db5c3d60dafd8576949e7
Lando <lando@lando.test>
2026-01-14 21:29:39 +0000

    Bug 2009953: apply code formatting via Lando
    # ignore-this-changeset

--- accessible/base/nsAccessibilityService.h
+++ accessible/base/nsAccessibilityService.h
@@ -58,7 +58,7 @@ SelectionManager* SelectionMgr();
 ApplicationAccessible* ApplicationAcc();
 xpcAccessibleApplication* XPCApplicationAcc();
 
-typedef LocalAccessible*(New_Accessible)(mozilla::dom::Element* aElement,
+typedef LocalAccessible*(New_Accessible)(mozilla::dom::Element * aElement,
                                          LocalAccessible* aContext);
 
 // These fields are not `nsStaticAtom* const` because MSVC doesn't like it.
