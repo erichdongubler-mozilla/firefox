369f104fe6298c9b03ba3cec22e60e42fffc254a
Cristina Horotan <chorotan@mozilla.com>
2026-01-14 16:25:18 +0200

    Revert "Bug 2009336, Bug 2009335, Bug 2009334 - Add missing header guards in netwerk/ headers r=necko-reviewers,valentin" for causing bc failures on browser_glean_telemetry_bounce.js
    This reverts commit 3a18bc07bfcb111c5a92edc0d64f9fbeee84bd45.
    
    Revert "Bug 2009335 - Add missing header guards in mozglue/ headers r=emilio"
    
    This reverts commit 42694c0687177e052c1d7a1a1e415cc4a51dba4a.
    
    Revert "Bug 2009334 - Add missing header guards in mobile/ headers r=geckoview-reviewers,tcampbell"
    
    This reverts commit c8cc1ce9d1ee021e30ae02bbbbbce83cec5f156e.

--- netwerk/base/http-sfv/SFVService.h
+++ netwerk/base/http-sfv/SFVService.h
@@ -4,9 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef NETWERK_BASE_HTTP_SFV_SFVSERVICE_H_
-#define NETWERK_BASE_HTTP_SFV_SFVSERVICE_H_
-
 #include "nsIStructuredFieldValues.h"
 namespace mozilla {
 namespace net {
@@ -15,5 +12,3 @@ already_AddRefed<nsISFVService> GetSFVService();
 
 }  // namespace net
 }  // namespace mozilla
-
-#endif  // NETWERK_BASE_HTTP_SFV_SFVSERVICE_H_
--- netwerk/protocol/http/ReplacedHttpResponse.h
+++ netwerk/protocol/http/ReplacedHttpResponse.h
@@ -4,9 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef NETWERK_PROTOCOL_HTTP_REPLACEDHTTPRESPONSE_H_
-#define NETWERK_PROTOCOL_HTTP_REPLACEDHTTPRESPONSE_H_
-
 #include "nsString.h"
 #include "nsHttpHeaderArray.h"
 #include "nsIReplacedHttpResponse.h"
@@ -30,5 +27,3 @@ class ReplacedHttpResponse : nsIReplacedHttpResponse {
 };
 
 }  // namespace mozilla::net
-
-#endif  // NETWERK_PROTOCOL_HTTP_REPLACEDHTTPRESPONSE_H_
