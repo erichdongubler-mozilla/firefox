9272ce026ede859850db813b2ea35623008982b2
serge-sans-paille <sguelton@mozilla.com>
2026-01-14 22:25:37 +0000

    Bug 2009953 - Do not use reserved identifiers for macro guard in layout r=firefox-style-system-reviewers,layout-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D278787

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -6,8 +6,8 @@
 
 /* a presentation of a document, part 1 */
 
-#ifndef nsPresContext_h___
-#define nsPresContext_h___
+#ifndef nsPresContext_h_
+#define nsPresContext_h_
 
 #include "FontVisibilityProvider.h"
 #include "Units.h"
@@ -1495,4 +1495,4 @@ class nsRootPresContext final : public nsPresContext {
 #  define DO_GLOBAL_REFLOW_COUNT(_name)
 #endif  // MOZ_REFLOW_PERF
 
-#endif /* nsPresContext_h___ */
+#endif /* nsPresContext_h_ */
