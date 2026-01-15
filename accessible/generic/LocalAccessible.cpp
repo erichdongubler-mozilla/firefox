721eb271542bc07f059e054816f76ecec9204dc9
Cristian Tuns <ctuns@mozilla.com>
2026-01-15 03:26:12 -0500

    Revert "Bug 2009757: Include the value of color inputs in the RemoteAccessible cache. Fire the correct event when the value changes. r=eeejay,layout-reviewers,emilio" for causing build bustages in LocalAccessible.cpp
    This reverts commit 7a935e6becc3fb3790aa8e39fe60169d73071443.

--- accessible/generic/LocalAccessible.cpp
+++ accessible/generic/LocalAccessible.cpp
@@ -3439,12 +3439,11 @@ already_AddRefed<AccAttributes> LocalAccessible::BundleFieldsForCache(
   }
 
   if (aCacheDomain & CacheDomain::Value) {
-    // We cache the text value in 5 cases:
+    // We cache the text value in 3 cases:
     // 1. Accessible is an HTML input type that holds a number.
     // 2. Accessible has a numeric value and an aria-valuetext.
     // 3. Accessible is an HTML input type that holds text.
     // 4. Accessible is a link, in which case value is the target URL.
-    // 5. Accessible is an HTML input type that holds a color.
     // ... for all other cases we divine the value remotely.
     bool cacheValueText = false;
     if (HasNumericValue()) {
@@ -3456,10 +3455,8 @@ already_AddRefed<AccAttributes> LocalAccessible::BundleFieldsForCache(
                        (mContent->IsElement() &&
                         nsAccUtils::HasARIAAttr(mContent->AsElement(),
                                                 nsGkAtoms::aria_valuetext));
-    } else if (IsTextField() || IsHTMLLink()) {
-      cacheValueText = true;
-    } else if (auto* input = dom::HTMLInputElement::FromNode(mContent)) {
-      cacheValueText = input->IsInputColor();
+    } else {
+      cacheValueText = IsTextField() || IsHTMLLink();
     }
 
     if (cacheValueText) {
