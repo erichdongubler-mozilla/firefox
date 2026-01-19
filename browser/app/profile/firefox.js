e2d3c7f50a974f79c454343e0c7c47d800586c20
Tom Schuster <tschuster@mozilla.com>
2026-01-19 13:45:50 +0000

    Bug 2006015 - Enable browser.tabs.remoteSVGIconDecoding by default. r=tabbrowser-reviewers,dao
    Differential Revision: https://phabricator.services.mozilla.com/D278288

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2700,7 +2700,7 @@ pref("browser.tabs.fadeOutUnloadedTabs", false);
 #endif
 
 // Whether SVG favicons should be safely re-encoded using the moz-remote-image:// protocol.
-pref("browser.tabs.remoteSVGIconDecoding", false);
+pref("browser.tabs.remoteSVGIconDecoding", true);
 
 // If true, unprivileged extensions may use experimental APIs on
 // nightly and developer edition.
