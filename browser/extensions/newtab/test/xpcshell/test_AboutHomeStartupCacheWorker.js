3542fdd337617fa326a77f477c7c41bcf86dfe15
agoloman <agoloman@mozilla.com>
2026-01-22 03:15:37 +0200

    Revert "Bug 2009017 - Part 3: Make StartupCacheInit feed start first amongst feeds. r=thecount,home-newtab-reviewers,nbarrett" for causing bcv failures.
    This reverts commit cdc9349c281ddaf4138f4baae13c245ca8dfe2f1.
    
    Revert "Bug 2009017 - Part 2: Compute instant of profile creation and have ActivityStream cache it. r=home-newtab-reviewers,nbarrett"
    
    This reverts commit 23c94dce1dbb78c666bc6e8bd8c6ea227fec0e01.
    
    Revert "Bug 2009017 - Part 1: Block initializing ActivityStream on the newtabTrainhop Nimbus feature being ready. r=nbarrett,home-newtab-reviewers"
    
    This reverts commit b9992564382d41cbea3174eb5724a1723c7c747f.

--- browser/extensions/newtab/test/xpcshell/test_AboutHomeStartupCacheWorker.js
+++ browser/extensions/newtab/test/xpcshell/test_AboutHomeStartupCacheWorker.js
@@ -18,9 +18,6 @@ const { TestUtils } = ChromeUtils.importESModule(
 const { sinon } = ChromeUtils.importESModule(
   "resource://testing-common/Sinon.sys.mjs"
 );
-const { NimbusTestUtils } = ChromeUtils.importESModule(
-  "resource://testing-common/NimbusTestUtils.sys.mjs"
-);
 
 SearchTestUtils.init(this);
 
@@ -36,8 +33,6 @@ ChromeUtils.defineESModuleGetters(this, {
 
 const CACHE_WORKER_URL = "resource://newtab/lib/cache.worker.js";
 const NEWTAB_RENDER_URL = "resource://newtab/data/content/newtab-render.js";
-
-NimbusTestUtils.init(this);
 
 /**
  * In order to make this test less brittle, much of Activity Stream is
@@ -116,12 +111,9 @@ add_setup(async function () {
     { setAsDefault: true }
   );
 
-  const { cleanup: nimbusTestCleanup } = await NimbusTestUtils.setupTest();
-  registerCleanupFunction(nimbusTestCleanup);
-
   // Pretend that a new window has been loaded to kick off initializing all of
   // the feeds.
-  await AboutNewTab.onBrowserReady();
+  AboutNewTab.onBrowserReady();
 
   // Much of Activity Stream initializes asynchronously. This is the easiest way
   // I could find to ensure that enough of the feeds had initialized to produce
