9cf0cdca48f4a16a3c353f162cda7096bf7e0dae
agoloman <agoloman@mozilla.com>
2026-01-23 11:02:28 +0200

    Revert "Bug 1186210 - Delete expired Necko and LocalStorage histograms r=necko-reviewers,toolkit-telemetry-reviewers,kershaw,dom-storage-reviewers,jesup,janv" for causing build bustages @LocalStorageCache.
    This reverts commit 245f8ca52ddd728718fb9c345b048786b73ffe4f.

--- dom/storage/LocalStorageCache.cpp
+++ dom/storage/LocalStorageCache.cpp
@@ -235,6 +235,11 @@ void LocalStorageCache::WaitForPreload() {
   // Telemetry of rates of pending preloads
   if (!mPreloadTelemetryRecorded) {
     mPreloadTelemetryRecorded = true;
+    glean::localdomstorage::preload_pending_on_first_access
+        .EnumGet(static_cast<
+                 glean::localdomstorage::PreloadPendingOnFirstAccessLabel>(
+            !loaded))
+        .Add();
   }
 
   if (loaded) {
