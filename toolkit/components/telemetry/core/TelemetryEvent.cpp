14b4722c7d209bc4e9cf6604ad2c5cdf669f2d4f
Cosmin Sabou <csabou@mozilla.com>
2026-01-28 18:43:37 +0200

    Revert "Bug 2012188 - Remove Legacy Telemetry event summarization r=TravisLong,extension-reviewers,omc-reviewers,emcminn,rpl" for causing non-unified build bustages on TelemetryScalar
    This reverts commit 44572f04f41e3f319754615234d6d648bbfc7f06.

--- toolkit/components/telemetry/core/TelemetryEvent.cpp
+++ toolkit/components/telemetry/core/TelemetryEvent.cpp
@@ -474,6 +474,10 @@ RecordEventResult RecordEvent(const StaticMutexAutoLock& lock,
     return RecordEventResult::CannotRecord;
   }
 
+  // Count the number of times this event has been recorded.
+  TelemetryScalar::SummarizeEvent(UniqueEventName(category, method, object),
+                                  processType);
+
   EventRecordArray* eventRecords = GetEventRecordsForProcess(lock, processType);
   eventRecords->AppendElement(EventRecord(timestamp, eventKey, value, extra));
 
