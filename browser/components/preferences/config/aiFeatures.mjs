5b2122684a1dcc782656e45857c571b27be79c4a
Mark Striemer <mstriemer@mozilla.com>
2026-01-29 20:16:05 +0000

    Bug 2005805 - Respect isAllowed and isManagedByPolicy for AI Controls  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D280317
    
    Differential Revision: https://phabricator.services.mozilla.com/D281011

--- browser/components/preferences/config/aiFeatures.mjs
+++ browser/components/preferences/config/aiFeatures.mjs
@@ -146,6 +146,12 @@ function makeAiControlSetting({ id, pref, feature, supportsEnabled = true }) {
       }
       return prefVal;
     },
+    disabled() {
+      return OnDeviceModelManager.isManagedByPolicy(feature);
+    },
+    visible() {
+      return OnDeviceModelManager.isAllowed(feature);
+    },
   });
 }
 makeAiControlSetting({
@@ -230,6 +236,12 @@ Preferences.addSetting(
         OnDeviceModelManager.enable(this.feature);
       }
       return AiControlStates.enabled;
+    },
+    disabled() {
+      return OnDeviceModelManager.isManagedByPolicy(this.feature);
+    },
+    visible() {
+      return OnDeviceModelManager.isAllowed(this.feature);
     },
     getControlConfig(config, _, setting) {
       let providerUrl = setting.value;
