06b645cc510fd220fa13aa697c733a95435ae961
ffxbld <ffxbld@mozilla.com>
2026-01-29 17:19:42 +0000

    No Bug, mozilla-beta repo-update HSTS HPKP mobile-experiments ct-logs -  a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D280914

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -681,4 +681,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1777892040333000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1778151314534000);
