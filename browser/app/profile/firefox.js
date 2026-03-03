cab347bb8ecc76bcd374b3e682aabbd62a4a17e3
smayya <smayya@mozilla.com>
2026-03-03 08:54:04 +0000

    Bug 2017207 - increase temproary permission expiry to 24 hours for Local network access. r=emz,necko-reviewers,permissions-reviewers,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D283633

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1369,6 +1369,9 @@ pref("network.manage-offline-status", true);
 // timeout for local network access prompts
 pref("network.lna.prompt.timeout", 300000); // 5 minutes
 
+// expiration time for temporary local network access permissions
+pref("network.lna.temporary_permission_expire_time_ms", 86400000); // 24 hours
+
 // We want to make sure mail URLs are handled externally...
 pref("network.protocol-handler.external.mailto", true); // for mail
 
