a3099109d94f0f9afa08d6cb6e6cd0122e3caef0
Sebastian Streich <sstreich@mozilla.com>
2026-03-03 16:50:42 +0000

    Bug 2016344 - Preamble, use dispose-symbols for browser/head.js:withProxyServer() r=ip-protection-reviewers,fchasen
    Differential Revision: https://phabricator.services.mozilla.com/D284516

--- browser/components/ipprotection/tests/browser/head.js
+++ browser/components/ipprotection/tests/browser/head.js
@@ -168,10 +168,12 @@ async function closePanel(win = window) {
  * Does not proxy anything really.
  * Given it refuses the proxy connection, it will be removed from as proxy-info of the channel.
  *
- * @param {*} testFn
- * @param {Function<Promise<void>>} handler - A custom path handler for "/" requests.
+ * Use with `await using` for automatic cleanup:
+ *   await using proxyInfo = withProxyServer();
+ *
+ * @param {Function} [handler] - A custom path handler for "/" and "CONNECT" requests.
  */
-async function withProxyServer(testFn, handler) {
+function withProxyServer(handler) {
   const server = new HttpServer();
   let { promise, resolve } = Promise.withResolvers();
 
@@ -217,7 +219,7 @@ async function withProxyServer(testFn, handler) {
   server.identity.add("http", "example.com", "443");
 
   server.start(-1);
-  await testFn({
+  return {
     server: new Server({
       hostname: "localhost",
       port: server.identity.primaryPort,
@@ -233,8 +235,10 @@ async function withProxyServer(testFn, handler) {
     }),
     type: "http",
     gotConnection: promise,
-  });
-  return server;
+    async [Symbol.asyncDispose]() {
+      await new Promise(r => server.stop(r));
+    },
+  };
 }
 /* exported withProxyServer */
 
