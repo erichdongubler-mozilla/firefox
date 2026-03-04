7e59786d3d7c42a6674593b72903b6aa5d975342
Andreas Farre <farre@mozilla.com>
2026-03-04 13:13:59 +0000

    Bug 2020364 - Part 1: Rename method tracker to navigation tracker. r=dom-core,jjaschke
    Differential Revision: https://phabricator.services.mozilla.com/D285523

--- docshell/base/nsDocShellLoadState.h
+++ docshell/base/nsDocShellLoadState.h
@@ -35,7 +35,7 @@ class OriginAttributes;
 namespace dom {
 class FormData;
 class DocShellLoadStateInit;
-struct NavigationAPIMethodTracker;
+struct NavigationTracker;
 }  // namespace dom
 }  // namespace mozilla
 
@@ -446,10 +446,8 @@ class nsDocShellLoadState final {
   // This is used to pass the navigation API method tracker through the
   // navigation pipeline for navigate().
   // See https://html.spec.whatwg.org/#navigation-api-method-tracker
-  mozilla::dom::NavigationAPIMethodTracker* GetNavigationAPIMethodTracker()
-      const;
-  void SetNavigationAPIMethodTracker(
-      mozilla::dom::NavigationAPIMethodTracker* aTracker);
+  mozilla::dom::NavigationTracker* GetNavigationTracker() const;
+  void SetNavigationTracker(mozilla::dom::NavigationTracker* aTracker);
 
   // This is used as the parameter for https://html.spec.whatwg.org/#navigate
   mozilla::dom::NavigationType GetNavigationType() const;
@@ -749,7 +747,7 @@ class nsDocShellLoadState final {
 
   RefPtr<nsStructuredCloneContainer> mNavigationAPIState;
 
-  RefPtr<mozilla::dom::NavigationAPIMethodTracker> mNavigationAPIMethodTracker;
+  RefPtr<mozilla::dom::NavigationTracker> mNavigationTracker;
 
   RefPtr<mozilla::dom::FormData> mFormDataEntryList;
 
