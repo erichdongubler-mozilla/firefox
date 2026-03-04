fa33a51e85e685e2b7863ee880a6b4825599cd18
Tom Schuster <tschuster@mozilla.com>
2026-03-04 14:25:56 +0000

    Bug 2016915 - Basic CSP support for Worklets. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D284245

--- dom/security/OffThreadCSPContext.h
+++ dom/security/OffThreadCSPContext.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_workers_WorkerCSPContext_h_
-#define mozilla_dom_workers_WorkerCSPContext_h_
+#ifndef mozilla_dom_OffThreadCSPContext_h_
+#define mozilla_dom_OffThreadCSPContext_h_
 
 #include "mozilla/Result.h"
 #include "mozilla/UniquePtr.h"
@@ -15,12 +15,13 @@ class nsIContentSecurityPolicy;
 
 namespace mozilla::dom {
 
-// A minimal version of nsCSPContext that can run on Worker threads.
-class WorkerCSPContext final {
+// A minimal version of nsCSPContext that can run on worker and worklet threads.
+class OffThreadCSPContext final {
  public:
-  explicit WorkerCSPContext(mozilla::ipc::CSPInfo&& aInfo) : mCSPInfo(aInfo) {}
+  explicit OffThreadCSPContext(mozilla::ipc::CSPInfo&& aInfo)
+      : mCSPInfo(aInfo) {}
 
-  static Result<UniquePtr<WorkerCSPContext>, nsresult> CreateFromCSP(
+  static Result<UniquePtr<OffThreadCSPContext>, nsresult> CreateFromCSP(
       nsIContentSecurityPolicy* aCSP);
 
   const mozilla::ipc::CSPInfo& CSPInfo() const { return mCSPInfo; }
@@ -35,15 +36,14 @@ class WorkerCSPContext final {
   // Thread boundaries require us to not only store a CSP object, but also a
   // serialized version of the CSP. Reason being: Serializing a CSP to a CSPInfo
   // needs to happen on the main thread, but storing the CSPInfo needs to happen
-  // on the worker thread. We move the CSPInfo into the Client within
-  // ScriptExecutorRunnable::PreRun().
+  // on the worker/worklet thread.
   mozilla::ipc::CSPInfo mCSPInfo;
 
-  // This is created lazily by parsing the policies in CSPInfo on the worker
-  // thread.
+  // This is created lazily by parsing the policies in CSPInfo on the
+  // worker/worklet thread.
   nsTArray<UniquePtr<const nsCSPPolicy>> mPolicies;
 };
 
 }  // namespace mozilla::dom
 
-#endif  // mozilla_dom_workers_WorkerCSPContext_h_
+#endif  // mozilla_dom_OffThreadCSPContext_h_
