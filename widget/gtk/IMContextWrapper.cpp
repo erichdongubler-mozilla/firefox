8c3267e5b14633a3a90145599e4da991145767d6
stransky <stransky@redhat.com>
2026-03-05 19:39:00 +0000

    Bug 2018253 [Linux] Use nsWindow::FromWidget() instead of static_cast<nsWindow*> at widget/gtk r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D286269

--- widget/gtk/IMContextWrapper.cpp
+++ widget/gtk/IMContextWrapper.cpp
@@ -3031,7 +3031,7 @@ void IMContextWrapper::SetCursorPosition(GtkIMContext* aContext) {
   }
 
   nsWindow* rootWindow =
-      static_cast<nsWindow*>(mLastFocusedWindow->GetTopLevelWidget());
+      nsWindow::FromWidget(mLastFocusedWindow->GetTopLevelWidget());
 
   // Get the position of the rootWindow in screen.
   LayoutDeviceIntPoint root = rootWindow->WidgetToScreenOffset();
