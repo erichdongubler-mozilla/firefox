644a9ce730a183a525fff3dd27cbc82dab484036
Boris Chiou <boris.chiou@gmail.com>
2026-03-05 22:29:53 +0000

    Bug 2014914 - Part 5: Define the default values for StyleGenericAnimationRangeValue. r=firefox-style-system-reviewers,emilio
    Make the code slightly simpler.
    
    Differential Revision: https://phabricator.services.mozilla.com/D286194

--- dom/animation/Animation.h
+++ dom/animation/Animation.h
@@ -45,10 +45,8 @@ class Promise;
 
 // The helper struct to hold the animation-range values.
 struct AnimationRange {
-  StyleAnimationRangeStart mStart = {StyleTimelineRangeName::Normal,
-                                     LengthPercentage::FromPercentage(0.0f)};
-  StyleAnimationRangeEnd mEnd = {StyleTimelineRangeName::Normal,
-                                 LengthPercentage::FromPercentage(1.0f)};
+  StyleAnimationRangeStart mStart = StyleAnimationRangeStart::DefaultStart();
+  StyleAnimationRangeEnd mEnd = StyleAnimationRangeEnd::DefaultEnd();
   bool operator==(const AnimationRange& aOther) const {
     return mStart == aOther.mStart && mEnd == aOther.mEnd;
   }
