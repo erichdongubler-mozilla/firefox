0cdf754a4f9d63e3e21c35aa77590fb171e62e3b
Andreas Pehrson <apehrson@mozilla.com>
2026-03-09 13:52:04 +0000

    Bug 2012483 - Move responsibility for starting and stopping VideoCaptureImpls to AggregateCapturer::SetConfigurationFor. r=jib
    AggregateCapturer knows of all streams and their state for a capture device, so
    is better suited to decide when to start and stop, than the layer above.
    
    Differential Revision: https://phabricator.services.mozilla.com/D279879

--- dom/media/systemservices/CamerasParent.h
+++ dom/media/systemservices/CamerasParent.h
@@ -68,12 +68,11 @@ class AggregateCapturer final
   RemoveStreamResult RemoveStream(int aStreamId);
   RemoveStreamResult RemoveStreamsFor(CamerasParent* aParent);
   Maybe<int> CaptureIdFor(int aStreamId);
-  void SetConfigurationFor(int aStreamId,
-                           const webrtc::VideoCaptureCapability& aCapability,
-                           const NormalizedConstraints& aConstraints,
-                           const dom::VideoResizeModeEnum& aResizeMode,
-                           bool aStarted);
-  Maybe<webrtc::VideoCaptureCapability> CombinedCapability();
+  int32_t StartStream(int aStreamId,
+                      const webrtc::VideoCaptureCapability& aCapability,
+                      const NormalizedConstraints& aConstraints,
+                      const dom::VideoResizeModeEnum& aResizeMode);
+  int32_t StopStream(int aStreamId);
 
   void OnCaptureEnded();
   void OnFrame(const webrtc::VideoFrame& aVideoFrame) override;
@@ -101,7 +100,7 @@ class AggregateCapturer final
     // Whether the stream has been started and not stopped. As opposed to
     // allocated and not deallocated, which controls the presence of this stream
     // altogether.
-    bool mStarted{false};
+    bool mActive{false};
     // The timestamp of the last frame sent to mParent for this stream.
     media::TimeUnit mLastFrameTime{media::TimeUnit::FromNegativeInfinity()};
   };
@@ -132,6 +131,11 @@ class AggregateCapturer final
                     CaptureEngine aCapEng, VideoEngine* aEngine,
                     const nsCString& aUniqueId, int aCaptureId,
                     nsTArray<webrtc::VideoCaptureCapability>&& aCapabilities);
+
+  Maybe<webrtc::VideoCaptureCapability> CombinedCapability(
+      const decltype(mStreams)::AutoLock& aStreamsGuard);
+
+  int32_t UpdateDevice(const Maybe<webrtc::VideoCaptureCapability>& aState);
 
   MediaEventListener mCaptureEndedListener;
 };
