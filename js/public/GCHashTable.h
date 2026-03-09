eccb070a0fec43a087342b4e88cd3f081b0174cb
Jon Coppeard <jcoppeard@mozilla.com>
2026-03-09 09:20:15 +0000

    Bug 2021241 - Part 1: Add Iterator APIs to GCHashTable.h and related classes r=spidermonkey-reviewers,sfink
    Differential Revision: https://phabricator.services.mozilla.com/D286218

--- js/public/GCHashTable.h
+++ js/public/GCHashTable.h
@@ -84,32 +84,32 @@ class GCHashMap : public js::HashMap<Key, Value, HashPolicy, AllocPolicy> {
 
   void trace(JSTracer* trc, js::gc::Cell* owner = nullptr) {
     js::TraceOwnedAllocs(trc, owner, *this, "hashmap storage");
-    for (typename Base::Enum e(*this); !e.empty(); e.popFront()) {
-      GCPolicy<Value>::trace(trc, &e.front().value(), "hashmap value");
-      GCPolicy<Key>::trace(trc, &e.front().mutableKey(), "hashmap key");
+    for (auto iter = this->modIter(); !iter.done(); iter.next()) {
+      GCPolicy<Value>::trace(trc, &iter.get().value(), "hashmap value");
+      GCPolicy<Key>::trace(trc, &iter.get().mutableKey(), "hashmap key");
     }
   }
 
   bool traceWeak(JSTracer* trc) {
-    typename Base::Enum e(*this);
-    traceWeakEntries(trc, e);
+    auto iter = this->modIter();
+    traceWeakEntries(trc, iter);
     Base::compact();
     return !this->empty();
   }
 
-  void traceWeakEntries(JSTracer* trc, typename Base::Enum& e) {
-    for (; !e.empty(); e.popFront()) {
-      if (!MapEntryGCPolicy::traceWeak(trc, &e.front().mutableKey(),
-                                       &e.front().value())) {
-        e.removeFront();
+  void traceWeakEntries(JSTracer* trc, typename Base::ModIterator& iter) {
+    for (; !iter.done(); iter.next()) {
+      if (!MapEntryGCPolicy::traceWeak(trc, &iter.get().mutableKey(),
+                                       &iter.get().value())) {
+        iter.remove();
       }
     }
   }
 
   bool needsSweep(JSTracer* trc) const {
-    for (auto r = this->all(); !r.empty(); r.popFront()) {
-      if (MapEntryGCPolicy::needsSweep(trc, &r.front().key(),
-                                       &r.front().value())) {
+    for (auto iter = this->iter(); !iter.done(); iter.next()) {
+      if (MapEntryGCPolicy::needsSweep(trc, &iter.get().key(),
+                                       &iter.get().value())) {
         return true;
       }
     }
@@ -152,12 +152,12 @@ class GCRekeyableHashMap : public JS::GCHashMap<Key, Value, HashPolicy,
       : Base(std::move(a), length) {}
 
   bool traceWeak(JSTracer* trc) {
-    for (typename Base::Enum e(*this); !e.empty(); e.popFront()) {
-      Key key(e.front().key());
-      if (!MapEntryGCPolicy::traceWeak(trc, &key, &e.front().value())) {
-        e.removeFront();
-      } else if (!HashPolicy::match(key, e.front().key())) {
-        e.rekeyFront(key);
+    for (auto iter = this->modIter(); !iter.done(); iter.next()) {
+      Key key(iter.get().key());
+      if (!MapEntryGCPolicy::traceWeak(trc, &key, &iter.get().value())) {
+        iter.remove();
+      } else if (!HashPolicy::match(key, iter.get().key())) {
+        iter.rekey(key);
       }
     }
     return !this->empty();
@@ -181,9 +181,11 @@ class WrappedPtrOperations<JS::GCHashMap<Args...>, Wrapper> {
  public:
   using AddPtr = typename Map::AddPtr;
   using Ptr = typename Map::Ptr;
+  using Iterator = typename Map::Iterator;
   using Range = typename Map::Range;
 
   Ptr lookup(const Lookup& l) const { return map().lookup(l); }
+  Iterator iter() const { return map().iter(); }
   Range all() const { return map().all(); }
   bool empty() const { return map().empty(); }
   uint32_t count() const { return map().count(); }
@@ -207,6 +209,7 @@ class MutableWrappedPtrOperations<JS::GCHashMap<Args...>, Wrapper>
 
  public:
   using AddPtr = typename Map::AddPtr;
+  using ModIterator = typename Map::ModIterator;
   struct Enum : public Map::Enum {
     explicit Enum(Wrapper& o) : Map::Enum(o.map()) {}
   };
@@ -215,6 +218,7 @@ class MutableWrappedPtrOperations<JS::GCHashMap<Args...>, Wrapper>
 
   void clear() { map().clear(); }
   void clearAndCompact() { map().clearAndCompact(); }
+  ModIterator modIter() { return map().modIter(); }
   void remove(Ptr p) { map().remove(p); }
   AddPtr lookupForAdd(const Lookup& l) { return map().lookupForAdd(l); }
 
@@ -281,29 +285,29 @@ class GCHashSet : public js::HashSet<T, HashPolicy, AllocPolicy> {
 
   void trace(JSTracer* trc, js::gc::Cell* owner = nullptr) {
     js::TraceOwnedAllocs(trc, owner, *this, "hashset storage");
-    for (typename Base::Enum e(*this); !e.empty(); e.popFront()) {
-      GCPolicy<T>::trace(trc, &e.mutableFront(), "hashset element");
+    for (auto iter = this->modIter(); !iter.done(); iter.next()) {
+      GCPolicy<T>::trace(trc, &iter.getMutable(), "hashset element");
     }
   }
 
   bool traceWeak(JSTracer* trc) {
-    typename Base::Enum e(*this);
-    traceWeakEntries(trc, e);
+    auto iter = this->modIter();
+    traceWeakEntries(trc, iter);
     Base::compact();
     return !this->empty();
   }
 
-  void traceWeakEntries(JSTracer* trc, typename Base::Enum& e) {
-    for (; !e.empty(); e.popFront()) {
-      if (!GCPolicy<T>::traceWeak(trc, &e.mutableFront())) {
-        e.removeFront();
+  void traceWeakEntries(JSTracer* trc, typename Base::ModIterator& iter) {
+    for (; !iter.done(); iter.next()) {
+      if (!GCPolicy<T>::traceWeak(trc, &iter.getMutable())) {
+        iter.remove();
       }
     }
   }
 
   bool needsSweep(JSTracer* trc) const {
-    for (auto r = this->all(); !r.empty(); r.popFront()) {
-      if (GCPolicy<T>::needsSweep(trc, &r.front())) {
+    for (auto iter = this->iter(); !iter.done(); iter.next()) {
+      if (GCPolicy<T>::needsSweep(trc, &iter.get())) {
         return true;
       }
     }
@@ -336,9 +340,11 @@ class WrappedPtrOperations<JS::GCHashSet<Args...>, Wrapper> {
   using AddPtr = typename Set::AddPtr;
   using Entry = typename Set::Entry;
   using Ptr = typename Set::Ptr;
+  using Iterator = typename Set::Iterator;
   using Range = typename Set::Range;
 
   Ptr lookup(const Lookup& l) const { return set().lookup(l); }
+  Iterator iter() const { return set().iter(); }
   Range all() const { return set().all(); }
   bool empty() const { return set().empty(); }
   uint32_t count() const { return set().count(); }
@@ -363,6 +369,7 @@ class MutableWrappedPtrOperations<JS::GCHashSet<Args...>, Wrapper>
  public:
   using AddPtr = typename Set::AddPtr;
   using Entry = typename Set::Entry;
+  using ModIterator = typename Set::ModIterator;
   struct Enum : public Set::Enum {
     explicit Enum(Wrapper& o) : Set::Enum(o.set()) {}
   };
@@ -371,6 +378,7 @@ class MutableWrappedPtrOperations<JS::GCHashSet<Args...>, Wrapper>
 
   void clear() { set().clear(); }
   void clearAndCompact() { set().clearAndCompact(); }
+  ModIterator modIter() { return set().modIter(); }
   [[nodiscard]] bool reserve(uint32_t len) { return set().reserve(len); }
   void remove(Ptr p) { set().remove(p); }
   void remove(const Lookup& l) { set().remove(l); }
@@ -438,10 +446,10 @@ class WeakCache<
   size_t traceWeak(JSTracer* trc, NeedsLock needsLock) override {
     size_t steps = map.count();
 
-    // Create an Enum and sweep the table entries.
-    mozilla::Maybe<typename Map::Enum> e;
-    e.emplace(map);
-    map.traceWeakEntries(trc, e.ref());
+    // Create an iterator and sweep the table entries.
+    mozilla::Maybe<typename Map::ModIterator> iter;
+    iter.emplace(map.modIter());
+    map.traceWeakEntries(trc, iter.ref());
 
     // Potentially take a lock while the Enum's destructor is called as this can
     // rehash/resize the table and access the store buffer.
@@ -449,7 +457,7 @@ class WeakCache<
     if (needsLock) {
       lock.emplace(trc->runtime());
     }
-    e.reset();
+    iter.reset();
 
     return steps;
   }
@@ -476,35 +484,53 @@ class WeakCache<
   using AddPtr = typename Map::AddPtr;
 
   // Iterator over the whole collection.
-  struct Range {
-    explicit Range(Self& self) : cache(self), range(self.map.all()) {
+  struct Iterator {
+    explicit Iterator(Self& self) : cache(self), iter(self.map.iter()) {
       settle();
     }
-    Range() = default;
+    Iterator() = default;
 
-    bool empty() const { return range.empty(); }
-    const Entry& front() const { return range.front(); }
+    bool done() const { return iter.done(); }
+    const Entry& get() const { return iter.get(); }
 
-    void popFront() {
-      range.popFront();
+    void next() {
+      iter.next();
       settle();
     }
 
    private:
     Self& cache;
-    typename Map::Range range;
+    typename Map::Iterator iter;
 
     void settle() {
       if (JSTracer* trc = cache.barrierTracer) {
-        while (!empty() && entryNeedsSweep(trc, front())) {
-          popFront();
+        while (!done() && entryNeedsSweep(trc, get())) {
+          next();
         }
       }
     }
   };
 
+  class Range {
+    Iterator iter;
+
+   public:
+    explicit Range(Self& self) : iter(self) {}
+    bool empty() const { return iter.done(); }
+    const Entry& front() const { return iter.get(); }
+    void popFront() { return iter.next(); }
+  };
+
   struct Enum : public Map::Enum {
     explicit Enum(Self& cache) : Map::Enum(cache.map) {
+      // This operation is not allowed while barriers are in place as we
+      // may also need to enumerate the set for sweeping.
+      MOZ_ASSERT(!cache.barrierTracer);
+    }
+  };
+
+  struct ModIterator : public Map::ModIterator {
+    explicit ModIterator(Self& cache) : Map::ModIterator(cache.map) {
       // This operation is not allowed while barriers are in place as we
       // may also need to enumerate the set for sweeping.
       MOZ_ASSERT(!cache.barrierTracer);
@@ -530,6 +556,8 @@ class WeakCache<
   }
 
   Range all() const { return Range(*const_cast<Self*>(this)); }
+  Iterator iter() const { return Iterator(*const_cast<Self*>(this)); }
+  ModIterator modIter() { return ModIterator(*this); }
 
   bool empty() const {
     // This operation is not currently allowed while barriers are in place
@@ -632,11 +660,11 @@ class WeakCache<GCHashSet<T, HashPolicy, AllocPolicy>> final
   size_t traceWeak(JSTracer* trc, NeedsLock needsLock) override {
     size_t steps = set.count();
 
-    // Create an Enum and sweep the table entries. It's not necessary to take
-    // the sweeping lock yet.
-    mozilla::Maybe<typename Set::Enum> e;
-    e.emplace(set);
-    set.traceWeakEntries(trc, e.ref());
+    // Create an iterator and sweep the table entries. It's not necessary to
+    // take the sweeping lock yet.
+    mozilla::Maybe<typename Set::ModIterator> iter;
+    iter.emplace(set.modIter());
+    set.traceWeakEntries(trc, iter.ref());
 
     // Destroy the Enum, potentially rehashing or resizing the table. Since this
     // can access the store buffer, we need to take a lock for this if we're
@@ -645,7 +673,7 @@ class WeakCache<GCHashSet<T, HashPolicy, AllocPolicy>> final
     if (needsLock) {
       lock.emplace(trc->runtime());
     }
-    e.reset();
+    iter.reset();
 
     return steps;
   }
@@ -689,35 +717,53 @@ class WeakCache<GCHashSet<T, HashPolicy, AllocPolicy>> final
   using AddPtr = typename Set::AddPtr;
 
   // Iterator over the whole collection.
-  struct Range {
-    explicit Range(Self& self) : cache(self), range(self.set.all()) {
+  struct Iterator {
+    explicit Iterator(Self& self) : cache(self), iter(self.set.iter()) {
       settle();
     }
-    Range() = default;
+    Iterator() = default;
 
-    bool empty() const { return range.empty(); }
-    const Entry& front() const { return range.front(); }
+    bool done() const { return iter.done(); }
+    const Entry& get() const { return iter.get(); }
 
-    void popFront() {
-      range.popFront();
+    void next() {
+      iter.next();
       settle();
     }
 
    private:
     Self& cache;
-    typename Set::Range range;
+    typename Set::Iterator iter;
 
     void settle() {
       if (JSTracer* trc = cache.barrierTracer) {
-        while (!empty() && entryNeedsSweep(trc, front())) {
-          popFront();
+        while (!done() && entryNeedsSweep(trc, get())) {
+          next();
         }
       }
     }
   };
 
+  class Range {
+    Iterator iter;
+
+   public:
+    explicit Range(Self& self) : iter(self) {}
+    bool empty() const { return iter.done(); }
+    const Entry& front() const { return iter.get(); }
+    void popFront() { return iter.next(); }
+  };
+
   struct Enum : public Set::Enum {
     explicit Enum(Self& cache) : Set::Enum(cache.set) {
+      // This operation is not allowed while barriers are in place as we
+      // may also need to enumerate the set for sweeping.
+      MOZ_ASSERT(!cache.barrierTracer);
+    }
+  };
+
+  struct ModIterator : public Set::ModIterator {
+    explicit ModIterator(Self& cache) : Set::ModIterator(cache.set.modIter()) {
       // This operation is not allowed while barriers are in place as we
       // may also need to enumerate the set for sweeping.
       MOZ_ASSERT(!cache.barrierTracer);
@@ -743,6 +789,8 @@ class WeakCache<GCHashSet<T, HashPolicy, AllocPolicy>> final
   }
 
   Range all() const { return Range(*const_cast<Self*>(this)); }
+  Iterator iter() const { return Iterator(*const_cast<Self*>(this)); }
+  ModIterator modIter() { return ModIterator(*this); }
 
   bool empty() const {
     // This operation is not currently allowed while barriers are in place
