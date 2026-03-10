58b31511093ce5a96c3cc2028efd1107edf4ef7b
rperta <rperta@mozilla.com>
2026-03-10 09:05:24 +0200

    Revert "Bug 2022003 - Add AnonymousInvertedHashKey() r=kershaw,valentin,necko-reviewers" for causing build bustages
    This reverts commit ef4a50c9afd813448c075ee042e3a0eb8f5096e6.

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -3550,9 +3550,9 @@ ConnectionEntry* nsHttpConnectionMgr::GetOrCreateConnectionEntry(
   // step 1 repeated for an inverted anonymous flag; we return an entry
   // only when it has an h2 established connection that is not authenticated
   // with a client certificate.
-  nsAutoCString anonInvertedKey;
-  specificCI->AnonymousInvertedHashKey(anonInvertedKey);
-  ConnectionEntry* invertedEnt = mCT.GetWeak(anonInvertedKey);
+  RefPtr<nsHttpConnectionInfo> anonInvertedCI(specificCI->Clone());
+  anonInvertedCI->SetAnonymous(!specificCI->GetAnonymous());
+  ConnectionEntry* invertedEnt = mCT.GetWeak(anonInvertedCI->HashKey());
   if (invertedEnt) {
     HttpConnectionBase* h2orh3conn =
         GetH2orH3ActiveConn(invertedEnt, aNoHttp2, aNoHttp3);
