8352e8a926b8c77d2706b4d318f0a2b17bd0eacd
Kui-Feng Lee <thinker.li@gmail.com>
2026-03-11 21:57:04 +0000

    Bug 2003137 - Create an empty profiles.ini in the legacy directory for TestXREAppDir.cpp. r=gerard-majax
    A legacy directory is valid/or existing only if profiles.ini is there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D278085

--- toolkit/tests/gtest/TestXREAppDir.cpp
+++ toolkit/tests/gtest/TestXREAppDir.cpp
@@ -145,6 +145,18 @@ class BaseXREAppDir : public ::testing::Test {
     mOriginalAppData = gAppData;
     gAppData = &mFakeAppData;
     EXPECT_STREQ(gAppData->profile, "fooprofile");
+#if defined(XP_UNIX) && !defined(XP_MACOSX)
+    // Create the profile directory and profiles.ini for
+    // AppDataProfile tests.  Since tests expect the name
+    // ".fooprofile", it should be a legacy directory. The
+    // prefixed dot is added only for the legacy profile directory.
+    nsCString profileDir;
+    MkHomeSubdir(".fooprofile", profileDir);
+    nsCString profilesIni = profileDir + "/profiles.ini"_ns;
+    FILE* fp = fopen(profilesIni.get(), "w");
+    ASSERT_NE(fp, nullptr);
+    fclose(fp);
+#endif
   }
 
   void SwitchFakeAppDataOff() {
@@ -301,6 +313,14 @@ class ExistentLegacyXREAppDir_Generic : public BaseXREAppDir {
     BaseXREAppDir::SetUp();
     MkHomeSubdir(".mozilla", mMozDir);
     MkHomeSubdir(".mozilla/firefox", mMozDir);
+#if defined(XP_UNIX) && !defined(XP_MACOSX)
+    // Create an empty profiles.ini file to indicate a legacy installation
+    nsCString profilesIni =
+        mMockedHomeDir + "/.mozilla/firefox/profiles.ini"_ns;
+    FILE* fp = fopen(profilesIni.get(), "w");
+    ASSERT_NE(fp, nullptr);
+    fclose(fp);
+#endif
   }
   nsCString mMozDir;
 };
