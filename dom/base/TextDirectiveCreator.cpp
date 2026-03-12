1a7f24e5b509e77acf1b5a3def8db15689bc0143
Masayuki Nakano <masayuki@d-toybox.com>
2026-03-12 02:59:33 +0000

    Bug 2012637 - part 5: Delete the default template parameter of `nsContentUtils::ComparePoints` and relatives r=smaug,dom-core
    They accept any kind of points even if it does not match with the
    template parameter's `TreeKind` because the caller wants to check the
    relation in different kind of tree.  Therefore, any callers need to
    consider the `TreeKind` more carefully and clarify the `TreeKind` makes
    developers easier to find a bug.
    
    Differential Revision: https://phabricator.services.mozilla.com/D281308

--- dom/base/TextDirectiveCreator.cpp
+++ dom/base/TextDirectiveCreator.cpp
@@ -142,7 +142,8 @@ TextDirectiveCreator::ExtendRangeToWordBoundaries(AbstractRange* aRange) {
   endPoint =
       TextDirectiveUtil::FindWordBoundary<TextScanDirection::Right>(endPoint);
 #if MOZ_DIAGNOSTIC_ASSERT_ENABLED
-  auto cmp = nsContentUtils::ComparePoints(startPoint, endPoint);
+  auto cmp = nsContentUtils::ComparePoints<TreeKind::ShadowIncludingDOM>(
+      startPoint, endPoint);
   MOZ_DIAGNOSTIC_ASSERT(
       cmp && *cmp != 1,
       "The new end point must not be before the start point.");
@@ -457,8 +458,8 @@ TextDirectiveCreator::FindAllMatchingRanges(const nsString& aSearchQuery,
       break;
     }
     searchStart = searchResult->StartRef();
-    if (auto cmp = nsContentUtils::ComparePoints(searchStart, aSearchEnd,
-                                                 &mNodeIndexCache);
+    if (auto cmp = nsContentUtils::ComparePoints<TreeKind::ShadowIncludingDOM>(
+            searchStart, aSearchEnd, &mNodeIndexCache);
         !cmp || *cmp != -1) {
       // this means hitting a bug in nsFind which apparently does not stop
       // exactly where it is told to. There are cases where it might
@@ -474,8 +475,8 @@ TextDirectiveCreator::FindAllMatchingRanges(const nsString& aSearchQuery,
         TextDirectiveUtil::MoveToNextBoundaryPoint(searchStart);
     MOZ_DIAGNOSTIC_ASSERT(newSearchStart != searchStart);
     searchStart = newSearchStart;
-    if (auto cmp = nsContentUtils::ComparePoints(searchStart, aSearchEnd,
-                                                 &mNodeIndexCache);
+    if (auto cmp = nsContentUtils::ComparePoints<TreeKind::ShadowIncludingDOM>(
+            searchStart, aSearchEnd, &mNodeIndexCache);
         !cmp || *cmp != -1) {
       break;
     }
