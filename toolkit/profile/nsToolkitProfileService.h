934fe6749141457659488389255a15410f1b6a8e
rperta <rperta@mozilla.com>
2026-05-15 12:59:59 +0300

    Revert "Bug 2038187: Record parsing errors from profiles.ini. r=profiles-reviewers,jhirsch" for causing xpc failures at test_iniParser.js
    This reverts commit 0553bc58825c24442224d77cf3de8ac24cfa9aea.
    
    This reverts commit 71b1ef8df0139caeccacb0c0d668490e1296d029.

--- toolkit/profile/nsToolkitProfileService.h
+++ toolkit/profile/nsToolkitProfileService.h
@@ -191,7 +191,6 @@ class nsToolkitProfileService final : public nsIToolkitProfileService {
   bool mProfileDBExists;
   int64_t mProfileDBFileSize;
   PRTime mProfileDBModifiedTime;
-  nsCString mIniStatus;
 
   // A background task queue for the async flushing operations.
   nsCOMPtr<nsISerialEventTarget> mAsyncQueue;
