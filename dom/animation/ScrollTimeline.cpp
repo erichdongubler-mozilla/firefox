49501542bed55eb3af354717e50d7f1fde288fc7
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2026-05-18 22:38:15 +0000

    Bug 2039783 - Treat ScrollTimeline source as root when it resolves to the document's scrolling element. r=layout-scroll-driven-animation-reviewers,dshin
    mIsRoot only tracked scroll(root), but the root element can be reached by
    other means, like scroll(nearest) and new ScrollTimeline({}).
    
    Fixes scroll-timeline-default-quirks-mode.html and
    animation-with-root-scroller.html.
    
    Differential Revision: https://phabricator.services.mozilla.com/D300696

--- dom/animation/ScrollTimeline.cpp
+++ dom/animation/ScrollTimeline.cpp
@@ -91,6 +91,18 @@ already_AddRefed<ScrollTimeline> ScrollTimeline::Constructor(
 
 Element* ScrollTimeline::GetSource() const { return SourceElement(); }
 
+ScrollTimeline::State ScrollTimeline::GetState() const {
+  const auto source = mScrollerInfo.Source();
+  // Use document.scrollingElement to tell whether it's the root scroll
+  // container. Note that we can't use mScrollerInfo.mType since Type::Nearest
+  // can also reach the root scroll container.
+  const bool isRoot =
+      source.mElement &&
+      source.mElement->OwnerDoc()->GetScrollingElementNoFlush() ==
+          source.mElement;
+  return State{source, mAxis, isRoot};
+}
+
 dom::ScrollAxis ScrollTimeline::GetScrollAxis() const {
   switch (mAxis) {
     case StyleScrollAxis::Block:
@@ -291,6 +303,8 @@ const ScrollContainerFrame* ScrollTimeline::State::GetScrollContainerFrame()
   }
 
   if (mIsRoot) {
+    // document.scrollingElement may point to <body> in quirks mode, but the
+    // root scroll container frame is what actually scrolls - return it.
     if (const PresShell* presShell = e->OwnerDoc()->GetPresShell()) {
       return presShell->GetRootScrollContainerFrame();
     }
