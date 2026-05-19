fd4daa5e3d0c477ae62816449e19c0edc6164dc3
Mark Striemer <mstriemer@mozilla.com>
2026-05-19 18:53:42 +0000

    Bug 2028609 - Enable Settings Redesign by default r=mkennedy
    Differential Revision: https://phabricator.services.mozilla.com/D300475

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3603,7 +3603,7 @@ pref("toolkit.rust-components.logging.crates", "");
 pref("toolkit.rust-components.logging.internal-level", "Warn");
 
 // Settings Redesign 2025 prefs
-pref("browser.settings-redesign.enabled", false);
+pref("browser.settings-redesign.enabled", true);
 
 // A preference that will be locked to reflect whether this build has support
 // for XDG Config Home handling. Mostly used to be able to keep tests around
