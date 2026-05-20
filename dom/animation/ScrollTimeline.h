bcee481fdabfce90252248c37ea6a9e08ff8c5e6
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2026-05-20 12:50:28 +0000

    Bug 2040070 - Drop stale MOZ_CAN_RUN_SCRIPT annotation from ScrollTimeline::GetSource. r=layout-scroll-driven-animation-reviewers,dshin
    Differential Revision: https://phabricator.services.mozilla.com/D300697

--- dom/animation/ScrollTimeline.h
+++ dom/animation/ScrollTimeline.h
@@ -204,8 +204,7 @@ class ScrollTimeline : public AnimationTimeline,
   static already_AddRefed<ScrollTimeline> Constructor(
       const GlobalObject& aGlobal, const ScrollTimelineOptions& aOptions,
       ErrorResult& aRv);
-  // MOZ_CAN_RUN_SCRIPT because GetScrollingElement may flush in quirks mode.
-  MOZ_CAN_RUN_SCRIPT Element* GetSource() const;
+  Element* GetSource() const;
   dom::ScrollAxis GetScrollAxis() const;
 
   State GetState() const;
