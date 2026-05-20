6237958608c9d89eed40fdeff787aa68e6ba9884
André Bargull <andre.bargull@gmail.com>
2026-05-20 19:14:29 +0000

    Bug 2041055 - Part 7: Use compare pseudo-instructions and NegateBoolean more. r=iain
    Prefer the pseudo-instructions because they make the code more readable. Also
    use the `NegateBoolean` helper in more cases.
    
    Differential Revision: https://phabricator.services.mozilla.com/D301599

--- js/src/jit/riscv64/MacroAssembler-riscv64.cpp
+++ js/src/jit/riscv64/MacroAssembler-riscv64.cpp
@@ -90,11 +90,11 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Imm32 imm,
     switch (c) {
       case Equal:
       case BelowOrEqual:
-        ma_sltu(dst, lhs, Operand(1));
+        seqz(dst, lhs);
         break;
       case NotEqual:
       case Above:
-        sltu(dst, zero, lhs);
+        snez(dst, lhs);
         break;
       case AboveOrEqual:
       case Below:
@@ -102,30 +102,30 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Imm32 imm,
         break;
       case GreaterThan:
       case LessThanOrEqual:
-        slt(dst, zero, lhs);
+        sgtz(dst, lhs);
         if (c == LessThanOrEqual) {
-          xori(dst, dst, 1);
+          NegateBool(dst, dst);
         }
         break;
       case LessThan:
       case GreaterThanOrEqual:
-        slt(dst, lhs, zero);
+        sltz(dst, lhs);
         if (c == GreaterThanOrEqual) {
-          xori(dst, dst, 1);
+          NegateBool(dst, dst);
         }
         break;
       case Zero:
-        ma_sltu(dst, lhs, Operand(1));
+        seqz(dst, lhs);
         break;
       case NonZero:
-        sltu(dst, zero, lhs);
+        snez(dst, lhs);
         break;
       case Signed:
-        slt(dst, lhs, zero);
+        sltz(dst, lhs);
         break;
       case NotSigned:
-        slt(dst, lhs, zero);
-        xori(dst, dst, 1);
+        sltz(dst, lhs);
+        NegateBool(dst, dst);
         break;
       default:
         MOZ_CRASH("Invalid condition.");
@@ -138,9 +138,9 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Imm32 imm,
     case NotEqual:
       ma_xor(dst, lhs, imm);
       if (c == Equal) {
-        ma_sltu(dst, dst, Operand(1));
+        seqz(dst, dst);
       } else {
-        sltu(dst, zero, dst);
+        snez(dst, dst);
       }
       break;
     case Zero:
@@ -152,7 +152,9 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Imm32 imm,
       Condition cond = ma_cmp(dst, lhs, imm, c);
       MOZ_ASSERT(cond == Equal || cond == NotEqual);
 
-      if (cond == Equal) xori(dst, dst, 1);
+      if (cond == Equal) {
+        NegateBool(dst, dst);
+      }
   }
 }
 
@@ -274,16 +276,16 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Register rhs,
     case Equal:
       // seq d,s,t =>
       //   xor d,s,t
-      //   sltiu d,d,1
+      //   seqz d,d
       xor_(dst, lhs, rhs);
-      ma_sltu(dst, dst, Operand(1));
+      seqz(dst, dst);
       break;
     case NotEqual:
       // sne d,s,t =>
       //   xor d,s,t
-      //   sltu d,$zero,d
+      //   snez d,d
       xor_(dst, lhs, rhs);
-      sltu(dst, zero, dst);
+      snez(dst, dst);
       break;
     case Above:
       // sgtu d,s,t =>
@@ -295,7 +297,7 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Register rhs,
       //   sltu d,s,t
       //   xori d,d,1
       sltu(dst, lhs, rhs);
-      xori(dst, dst, 1);
+      NegateBool(dst, dst);
       break;
     case Below:
       // sltu d,s,t
@@ -306,7 +308,7 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Register rhs,
       //   sltu d,t,s
       //   xori d,d,1
       sltu(dst, rhs, lhs);
-      xori(dst, dst, 1);
+      NegateBool(dst, dst);
       break;
     case GreaterThan:
       // sgt d,s,t =>
@@ -318,7 +320,7 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Register rhs,
       //   slt d,s,t
       //   xori d,d,1
       slt(dst, lhs, rhs);
-      xori(dst, dst, 1);
+      NegateBool(dst, dst);
       break;
     case LessThan:
       // slt d,s,t
@@ -329,31 +331,31 @@ void MacroAssemblerRiscv64::ma_cmp_set(Register dst, Register lhs, Register rhs,
       //   slt d,t,s
       //   xori d,d,1
       slt(dst, rhs, lhs);
-      xori(dst, dst, 1);
+      NegateBool(dst, dst);
       break;
     case Zero:
       MOZ_ASSERT(lhs == rhs);
       // seq d,s,$zero =>
-      //   sltiu d,s,1
-      ma_sltu(dst, lhs, Operand(1));
+      //   seqz d,s
+      seqz(dst, lhs);
       break;
     case NonZero:
       MOZ_ASSERT(lhs == rhs);
       // sne d,s,$zero =>
-      //   sltu d,$zero,s
-      sltu(dst, zero, lhs);
+      //   snez d,s
+      snez(dst, lhs);
       break;
     case Signed:
       MOZ_ASSERT(lhs == rhs);
-      slt(dst, lhs, zero);
+      sltz(dst, lhs);
       break;
     case NotSigned:
       MOZ_ASSERT(lhs == rhs);
       // sge d,s,$zero =>
-      //   slt d,s,$zero
+      //   sltz d,s
       //   xori d,d,1
-      slt(dst, lhs, zero);
-      xori(dst, dst, 1);
+      sltz(dst, lhs);
+      NegateBool(dst, dst);
       break;
     default:
       MOZ_CRASH("Invalid condition.");
@@ -5706,7 +5708,7 @@ void MacroAssemblerRiscv64::ma_sle(Register rd, Register rs, Operand rt) {
     ma_li(scratch, rt.immediate());
     slt(rd, scratch, rs);
   }
-  xori(rd, rd, 1);
+  NegateBool(rd, rd);
 }
 
 void MacroAssemblerRiscv64::ma_sleu(Register rd, Register rs, Operand rt) {
@@ -5721,7 +5723,7 @@ void MacroAssemblerRiscv64::ma_sleu(Register rd, Register rs, Operand rt) {
     ma_li(scratch, rt.immediate());
     sltu(rd, scratch, rs);
   }
-  xori(rd, rd, 1);
+  NegateBool(rd, rd);
 }
 
 void MacroAssemblerRiscv64::ma_sgt(Register rd, Register rs, Operand rt) {
@@ -5754,12 +5756,12 @@ void MacroAssemblerRiscv64::ma_sgtu(Register rd, Register rs, Operand rt) {
 
 void MacroAssemblerRiscv64::ma_sge(Register rd, Register rs, Operand rt) {
   ma_slt(rd, rs, rt);
-  xori(rd, rd, 1);
+  NegateBool(rd, rd);
 }
 
 void MacroAssemblerRiscv64::ma_sgeu(Register rd, Register rs, Operand rt) {
   ma_sltu(rd, rs, rt);
-  xori(rd, rd, 1);
+  NegateBool(rd, rd);
 }
 
 static inline bool IsZero(const Operand& rt) {
