faa92844c55a23e0a2c06dc6a0ebda4a0620bc7f
Kai Engert <kaie@kuix.de>
2026-05-21 13:32:38 +0000

    Bug 2038660 - Disable some chrome baseline CSP checks for Thunderbird. r=tschuster
    Differential Revision: https://phabricator.services.mozilla.com/D301742

--- dom/security/nsContentSecurityUtils.cpp
+++ dom/security/nsContentSecurityUtils.cpp
@@ -1909,6 +1909,8 @@ void nsContentSecurityUtils::AssertAboutPageHasCSP(Document* aDocument) {
 
 /* static */
 void nsContentSecurityUtils::AssertChromePageHasCSP(Document* aDocument) {
+  // Thunderbird's CSP does not pass these checks
+#  ifndef MOZ_THUNDERBIRD
   nsCOMPtr<nsIURI> documentURI = aDocument->GetDocumentURI();
   if (!documentURI->SchemeIs("chrome")) {
     return;
@@ -1939,8 +1941,6 @@ void nsContentSecurityUtils::AssertChromePageHasCSP(Document* aDocument) {
                             spec.get());
   }
 
-  // Thunderbird's CSP does not pass these checks.
-#  ifndef MOZ_THUNDERBIRD
   nsAutoString baselinePolicy;
   static_cast<nsCSPContext*>(csp.get())->GetPolicy(0)->toString(baselinePolicy);
   MOZ_ASSERT(baselinePolicy == kBaselineChromeCSP);
