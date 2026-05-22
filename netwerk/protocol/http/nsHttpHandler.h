6f746b2dc606e8ccd8a925d290127fa26a92c7df
Alexandru Marc <amarc@mozilla.com>
2026-05-22 07:37:05 +0300

    Revert "Bug 2036896: apply code formatting via Lando" for causing assertion failures @ nsThreadManager
    This reverts commit ca722fe1c4150ff6daa5a68522dbbccb563b7119.
    
    Revert "Bug 2036896: race tests for nsHttpHandler string members r=necko-reviewers,valentin"
    
    This reverts commit a790aa188c8c17b9c6a8e2a5ca430e701bd00c94.
    
    Revert "Bug 2036896: Fix nsHttpHandler accept-encoding and user-agent data races r=necko-reviewers,valentin"
    
    This reverts commit bc48290119de18d17b6e0da5f92908710ac93009.

--- netwerk/protocol/http/nsHttpHandler.h
+++ netwerk/protocol/http/nsHttpHandler.h
@@ -16,7 +16,6 @@
 #include "EventTokenBucket.h"
 
 #include "mozilla/DataMutex.h"
-#include "mozilla/EventTargetAndLockCapability.h"
 #include "mozilla/Mutex.h"
 #include "mozilla/StaticPtr.h"
 #include "mozilla/TimeStamp.h"
@@ -133,12 +132,7 @@ class nsHttpHandler final : public nsIHttpProtocolHandler,
   [[nodiscard]] nsresult AddConnectionHeader(nsHttpRequestHead*, uint32_t caps);
   bool IsAcceptableEncoding(const char* encoding, bool isSecure);
 
-  // Main-thread only; safe without the lock because PrefsChanged (the sole
-  // writer) also runs on the main thread.  Off-main-thread callers must use
-  // GetUserAgent() which acquires mUserAgentCap's lock.
-  const nsCString& UserAgent(bool aShouldResistFingerprinting)
-      MOZ_NO_THREAD_SAFETY_ANALYSIS;
-  void GetUserAgent(bool aShouldResistFingerprinting, nsCString& aOut);
+  const nsCString& UserAgent(bool aShouldResistFingerprinting);
 
   enum HttpVersion HttpVersion() { return mHttpVersion; }
   enum HttpVersion ProxyHttpVersion() { return mProxyHttpVersion; }
@@ -529,8 +523,7 @@ class nsHttpHandler final : public nsIHttpProtocolHandler,
   //
   // Useragent/prefs helper methods
   //
-  void BuildUserAgent() MOZ_REQUIRES(mUserAgentCap);
-  void RebuildUserAgent();
+  void BuildUserAgent();
   void InitUserAgentComponents();
 #ifdef XP_MACOSX
   void InitMSAuthorities();
@@ -633,10 +626,9 @@ class nsHttpHandler final : public nsIHttpProtocolHandler,
   nsCString mDocumentAcceptHeader;
 
   nsCString mAcceptLanguages;
-  Mutex mAcceptEncodingLock{"nsHttpHandler::AcceptEncoding"};
-  nsCString mHttpAcceptEncodings MOZ_GUARDED_BY(mAcceptEncodingLock);
-  nsCString mHttpsAcceptEncodings MOZ_GUARDED_BY(mAcceptEncodingLock);
-  nsCString mDictionaryAcceptEncodings MOZ_GUARDED_BY(mAcceptEncodingLock);
+  nsCString mHttpAcceptEncodings;
+  nsCString mHttpsAcceptEncodings;
+  nsCString mDictionaryAcceptEncodings;
 
   nsCString mDefaultSocketType;
 
@@ -661,11 +653,10 @@ class nsHttpHandler final : public nsIHttpProtocolHandler,
   nsCString mCompatDevice;
   nsCString mDeviceModelId;
 
-  mozilla::MainThreadAndLockCapability<mozilla::Mutex> mUserAgentCap{
-      "nsHttpHandler::UserAgent"};
-  nsCString mUserAgent MOZ_GUARDED_BY(mUserAgentCap);
+  nsCString mUserAgent;
   nsCString mSpoofedUserAgent;
-  nsCString mUserAgentOverride MOZ_GUARDED_BY(mUserAgentCap);
+  nsCString mUserAgentOverride;
+  bool mUserAgentIsDirty{true};  // true if mUserAgent should be rebuilt
   bool mAcceptLanguagesIsDirty{true};
 
   bool mPromptTempRedirect{true};
