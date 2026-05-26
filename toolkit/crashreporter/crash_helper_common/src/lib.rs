f6255c9baad4a06fc2de2269be5641e745311703
Simon Chopin <schopin@mozilla.com>
2026-05-26 11:46:08 +0000

    Bug 1985968 - Move the Rust generated annotation enum into a common crate r=gsvelto
    This will allow us to use them outside of crash_helper_server. Simply
    exposing the API as public wouldn't have sufficed as we want to use it
    in rust_minidump_writer_linux, and that would have resulted in a
    a dependency cycle.
    
    Differential Revision: https://phabricator.services.mozilla.com/D299707

--- toolkit/crashreporter/crash_helper_common/src/lib.rs
+++ toolkit/crashreporter/crash_helper_common/src/lib.rs
@@ -15,6 +15,10 @@ mod ipc_listener;
 mod ipc_queue;
 mod platform;
 
+pub mod crash_annotations {
+    include!(concat!(env!("OUT_DIR"), "/crash_annotations.rs"));
+}
+
 use bytes::Bytes;
 use messages::MessageError;
 
