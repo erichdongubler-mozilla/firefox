fa5fce81d2a7a08a91e5a3ccd6b4f3f2f690cae7
Sandor Molnar <smolnar@mozilla.com>
2026-05-27 16:28:29 +0300

    Revert "Bug 2040887 - Fix PreXULSkeletonUI profileArgIndex comparison and track shown state. r=mossop" backed out on request.
    This reverts commit daca5e5d9b40a343e10b0c5361f9f7127a51b258.

--- mozglue/misc/PreXULSkeletonUI.cpp
+++ mozglue/misc/PreXULSkeletonUI.cpp
@@ -1624,7 +1624,6 @@ static Result<Ok, PreXULSkeletonUIError> ValidateCmdlineArguments(
     }
 
     bool approved = false;
-    int j = 0;
     for (const char* approvedArg : approvedArguments) {
       // We do a case-insensitive compare here with _stricmp. Even though some
       // of these arguments are *not* read as case-insensitive, others *are*.
@@ -1636,12 +1635,11 @@ static Result<Ok, PreXULSkeletonUIError> ValidateCmdlineArguments(
       if (!_stricmp(flag, approvedArg)) {
         approved = true;
 
-        if (j == profileArgIndex) {
+        if (i == profileArgIndex) {
           *explicitProfile = true;
         }
         break;
       }
-      ++j;
     }
 
     if (!approved) {
@@ -2056,7 +2054,6 @@ static Result<Ok, PreXULSkeletonUIError> CreateAndStorePreXULSkeletonUIImpl(
       "CreatePreXULSkeletonUI", OTHER,
       MarkerTiming::IntervalUntilNowFrom(skeletonStart));
 
-  sPreXULSkeletonUIShown = true;
   return Ok();
 }
 
