c87eb69f7742ff4235251eaf01f08e0f5ebca511
stransky <stransky@redhat.com>
2026-05-27 12:40:51 +0000

    Bug 2025329 Update build config r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D294784

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -450,7 +450,6 @@ class nsIWidget : public nsSupportsWeakReference {
   using PopupLevel = mozilla::widget::PopupLevel;
   using BorderStyle = mozilla::widget::BorderStyle;
   using TransparencyMode = mozilla::widget::TransparencyMode;
-  using Screen = mozilla::widget::Screen;
 
   // Used in UpdateThemeGeometries.
   struct ThemeGeometry {
@@ -1172,7 +1171,7 @@ class nsIWidget : public nsSupportsWeakReference {
   /**
    * Return the screen the widget is in, or null if we don't know.
    */
-  virtual already_AddRefed<Screen> GetWidgetScreen();
+  virtual already_AddRefed<mozilla::widget::Screen> GetWidgetScreen();
 
   /**
    * Put the toplevel window into or out of fullscreen mode.
