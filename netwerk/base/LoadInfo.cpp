e11dfbe9555a6f1a372534915c47764ffa420f68
Lando <lando@lando.moz.tools>
2026-05-27 12:31:59 +0000

    Bug 1996495: apply code formatting via Lando
    # ignore-this-changeset

--- netwerk/base/LoadInfo.cpp
+++ netwerk/base/LoadInfo.cpp
@@ -2085,8 +2085,7 @@ void LoadInfo::UpdateParentAddressSpaceInfo() {
   if (!bc) {
     // For workers, read the IP address space from the policy container
     // which was propagated from the parent document.
-    if (mClientInfo.isSome() &&
-        mClientInfo->Type() != ClientType::Window) {
+    if (mClientInfo.isSome() && mClientInfo->Type() != ClientType::Window) {
       nsCOMPtr<nsIPolicyContainer> policyContainer = GetPolicyContainer();
       if (policyContainer) {
         mParentIpAddressSpace =
