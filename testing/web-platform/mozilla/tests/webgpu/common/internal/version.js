b673b592e92093364211eb710b5ccc9a3116747e
Andy Leiserson <aleiserson@mozilla.com>
2026-05-29 17:06:18 +0000

    Bug 2042580 - Update WebGPU CTS to 9a848e6eae8bc1efda4908fd3f2e194bebf8d2c7 r=webgpu-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D302672

--- testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
+++ testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '97b4991bf90f8fb13f51bffd8a6de9083d86286a';
+export const version = '9a848e6eae8bc1efda4908fd3f2e194bebf8d2c7';
