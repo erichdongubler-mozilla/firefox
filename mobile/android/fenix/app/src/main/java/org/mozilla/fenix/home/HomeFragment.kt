53a5224d65d620ef7574c8da555a0fb23fbbdb1f
Harrison Oglesby <oglesby.harrison@gmail.com>
2026-06-01 18:19:37 +0000

    Bug 2032541 - Add QR Scanning to Google Lens camera entry flow r=android-reviewers,petru
    Differential Revision: https://phabricator.services.mozilla.com/D298674

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -279,7 +279,11 @@ class HomeFragment : Fragment() {
         ViewBoundFeatureWrapper()
     private val lensLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            lensFeature?.get()?.handleImageResult(result.resultCode, result.data)
+            lensFeature?.get()?.handleCameraActivityResult(
+                result.resultCode,
+                result.data,
+                qrScanFenixFeature?.get(),
+            )
         }
     private val lensCameraPermissionLauncher: ActivityResultLauncher<String> =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
