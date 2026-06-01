312ba34b1e65f4d78eb9c10682cec98e8f3a1b46
Tom Schuster <tschuster@mozilla.com>
2026-06-01 09:30:50 +0000

    Bug 2039903 - Use nsIWidget::NativeModifiers for SynthesizeNativeMouseScrollEvent. r=masayuki,win-reviewers,gstoll
    Differential Revision: https://phabricator.services.mozilla.com/D301261

--- widget/headless/HeadlessWidget.cpp
+++ widget/headless/HeadlessWidget.cpp
@@ -436,8 +436,9 @@ nsresult HeadlessWidget::SynthesizeNativeMouseEvent(
 
 nsresult HeadlessWidget::SynthesizeNativeMouseScrollEvent(
     mozilla::LayoutDeviceIntPoint aPoint, uint32_t aNativeMessage,
-    double aDeltaX, double aDeltaY, double aDeltaZ, uint32_t aModifierFlags,
-    uint32_t aAdditionalFlags, nsISynthesizedEventCallback* aCallback) {
+    double aDeltaX, double aDeltaY, double aDeltaZ,
+    nsIWidget::NativeModifiers aModifierFlags, uint32_t aAdditionalFlags,
+    nsISynthesizedEventCallback* aCallback) {
   AutoSynthesizedEventCallbackNotifier notifier(aCallback);
   // The various platforms seem to handle scrolling deltas differently,
   // but the following seems to emulate it well enough.
