9e43ca8a718211cadea77b7098201efd157ce1a1
Nicolas Chevobbe <nchevobbe@mozilla.com>
2026-06-01 11:02:24 +0000

    Bug 2043637 - [devtools] Use private fields in markup-context.js. r=devtools-reviewers,ochameau
    Update tests that were using now private methods.
    
    Differential Revision: https://phabricator.services.mozilla.com/D303376

--- devtools/client/inspector/test/shared-head.js
+++ devtools/client/inspector/test/shared-head.js
@@ -837,7 +837,7 @@ function openStyleContextMenuAndGetAllItems(view, target) {
  * @return An array of MenuItems
  */
 function openContextMenuAndGetAllItems(inspector, options) {
-  const menu = inspector.markup.contextMenu._openMenu(options);
+  const menu = inspector.markup.contextMenu.openMenu(options);
   return buildContextMenuItems(menu);
 }
 
