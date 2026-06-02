c64d3505eec010755fa92013d7e93a61d75eb673
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2026-06-02 03:03:56 +0000

    Bug 2044030 - Add Noto Color Emoji Flags to emoji list of Android. r=jfkthame
    Some devices has Noto Color Emoji Flags like the following.
    
    ```xml
        <family lang="und-Zsye">
            <font weight="400" style="normal">NotoColorEmoji.ttf</font>
        </family>
        <family lang="und-Zsye">
            <font weight="400" style="normal">NotoColorEmojiFlags.ttf</font>
        </family>
    ```
    
    So it is better to add Noto Color Emoji Flags to emoji list of Android
    to display more emojis.
    
    Differential Revision: https://phabricator.services.mozilla.com/D303767

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2875,7 +2875,7 @@ pref("font.size.monospace.x-math", 13);
 #if defined(ANDROID)
   // We use the bundled Charis SIL Compact as serif font for Firefox for Android
 
-  pref("font.name-list.emoji", "SamsungColorEmoji, Noto Color Emoji");
+  pref("font.name-list.emoji", "SamsungColorEmoji, Noto Color Emoji, Noto Color Emoji Flags");
 
   pref("font.name-list.serif.ar", "Noto Naskh Arabic, Noto Serif, Droid Serif");
   pref("font.name-list.sans-serif.ar", "Noto Naskh Arabic, Roboto, Google Sans, Droid Sans");
