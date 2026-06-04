f1ed0b7d9aa169b46edae41602a1f5e768b8d492
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2026-06-04 09:48:13 +0000

    Bug 2041154 - Encapsulate the long-tap touchmove content-response condition in a TouchBlockState method. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D302254

--- gfx/layers/apz/src/InputQueue.cpp
+++ gfx/layers/apz/src/InputQueue.cpp
@@ -200,29 +200,13 @@ APZEventResult InputQueue::ReceiveTouchInput(
     result.SetStatusForFastFling(*block, aFlags, consumableFlags, target);
   } else {  // handling depends on ArePointerEventsConsumable()
     bool consumable = consumableFlags.IsConsumable();
-    const bool wasInSlop = block->IsInSlop();
+    TouchBlockState::InSlop wasInSlop = block->IsInSlop();
     if (block->UpdateSlopState(aEvent, consumable)) {
       INPQ_LOG("dropping event due to block %p being in %sslop\n", block.get(),
                consumable ? "" : "mini-");
       result.SetStatusAsConsumeNoDefault();
     } else {
-      // If all following conditions are met, we need to wait for a content
-      // response (again);
-      //  1) this is the first touch-move event bailing out from in-slop state
-      //     after a long-tap event has been fired
-      //  2) there's any APZ-aware event listeners
-      //  3) the event block hasn't yet been prevented
-      //
-      // An example scenario;
-      //  in the content there are two event listeners for `touchstart` and
-      //  `touchmove` respectively, and doing `preventDefault()` in the
-      //  `touchmove` event listener. Then if the user kept touching at a point
-      //  until a long-tap event happens, then if the user started moving their
-      // finger, we have to wait for a content response twice, one is for
-      // `touchstart` and one is for `touchmove`.
-      if (wasInSlop && aEvent.mType == MultiTouchInput::MULTITOUCH_MOVE &&
-          (block->WasLongTapProcessed() || block->IsWaitingLongTapResult()) &&
-          !block->IsTargetOriginallyConfirmed() && !block->ShouldDropEvents()) {
+      if (block->NeedsContentResponseAfterLongTap(aEvent, wasInSlop)) {
         INPQ_LOG(
             "bailing out from in-stop state in block %p after a long-tap "
             "happened\n",
@@ -247,8 +231,9 @@ APZEventResult InputQueue::ReceiveTouchInput(
   int32_t longTapTimeout = StaticPrefs::ui_click_hold_context_menus_delay();
   int32_t contentTimeout = StaticPrefs::apz_content_response_timeout();
   if (waitingForContentResponse && longTapTimeout < contentTimeout &&
-      block->IsInSlop() && GestureEventListener::IsLongTapEnabled()) {
-    MOZ_ASSERT(aEvent.mType == MultiTouchInput::MULTITOUCH_START);
+      bool(block->IsInSlop()) && GestureEventListener::IsLongTapEnabled()) {
+    MOZ_ASSERT(aEvent.mType == MultiTouchInput::MULTITOUCH_START ||
+               aInitialTouchMove == InitialTouchMove::Yes);
     MOZ_ASSERT(!block->IsDuringFastFling());
     RefPtr<Runnable> maybeLongTap = NewRunnableMethod<uint64_t>(
         "layers::InputQueue::MaybeLongTapTimeout", this,
@@ -865,7 +850,8 @@ void InputQueue::MainThreadTimeout(uint64_t aInputBlockId) {
     NS_WARNING("input block is not a cancelable block");
   }
   if (success) {
-    if (inputBlock->AsTouchBlock() && inputBlock->AsTouchBlock()->IsInSlop()) {
+    if (inputBlock->AsTouchBlock() &&
+        bool(inputBlock->AsTouchBlock()->IsInSlop())) {
       // If the touch block is still in slop, it's still possible this block
       // needs to send a touchmove to content after the long-press gesture
       // since preventDefault() in a touchmove event handler should stop
@@ -888,7 +874,7 @@ void InputQueue::MaybeLongTapTimeout(uint64_t aInputBlockId) {
 
   InputBlockState* inputBlock = FindBlockForId(aInputBlockId, nullptr);
   MOZ_ASSERT(!inputBlock || inputBlock->AsTouchBlock());
-  if (inputBlock && inputBlock->AsTouchBlock()->IsInSlop()) {
+  if (inputBlock && bool(inputBlock->AsTouchBlock()->IsInSlop())) {
     // If the block is still in slop, it won't have sent a touchmove to content
     // and so content will not have sent a content response. But also it means
     // the touchstart should trigger a long-press gesture so let's force the
