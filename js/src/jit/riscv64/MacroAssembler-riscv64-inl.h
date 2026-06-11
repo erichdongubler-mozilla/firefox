ccc650ad4c2cbc8f286cdcca8a18b3e8571335ab
André Bargull <andre.bargull@gmail.com>
2026-06-11 12:53:40 +0000

    Bug 2045021 - Part 7: Use Zfa instructions when applicable. r=jandem
    Also moves the scratch register for `RoundHelper`, so we don't unnecessarily
    reserve it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D304795

--- js/src/jit/riscv64/MacroAssembler-riscv64-inl.h
+++ js/src/jit/riscv64/MacroAssembler-riscv64-inl.h
@@ -1219,6 +1219,12 @@ void MacroAssembler::branchToComputedAddress(const BaseIndex& addr) {
 void MacroAssembler::branchTruncateDoubleMaybeModUint32(FloatRegister src,
                                                         Register dest,
                                                         Label* fail) {
+  // The Zfa fcvtmod.w.d instruction handles exactly this conversion.
+  if (HasZfaExtension()) {
+    fcvtmod_w_d(dest, src);
+    return;
+  }
+
   UseScratchRegisterScope temps(this);
 
   // Convert scalar to signed 64-bit fixed-point, rounding toward zero.
