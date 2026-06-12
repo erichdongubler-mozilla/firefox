f77bf80433d83cc3e627e307579e710c23d3f275
agoloman <agoloman@mozilla.com>
2026-06-12 09:21:18 +0300

    Revert "Bug 2020707, Bug 2020704 - Part 6: Update test expectations now that speculationrules scripts are supported. r=dom-core-reviewers,jjaschke" for causing bc failures @browser_misused_characters_in_strings.js.
    This reverts commit 524a1fc19181ecbd5b8c2a87682fc108de75d53a.
    
    Revert "Bug 2020707 - Part 5: Gate speculation rules script support behind a pref. r=dom-core-reviewers,jjaschke"
    
    This reverts commit 11741d18ad36ec606cbe0ab6bdd7ede7eac0696e.
    
    Revert "Bug 2020707 - Part 4: Unregister speculation rules when their script element is removed. r=dom-core-reviewers,hsivonen"
    
    This reverts commit 5202943dc9d1474723f413c7fc7e18093d45348b.
    
    Revert "Bug 2020707 - Part 3: Report an exception if speculation rules parsing fails. r=dom-core-reviewers,jjaschke"
    
    This reverts commit 70c5aa54253f63cef85a2a6dca68dec790249222.
    
    Revert "Bug 2020707 - Part 2: Add support for speculationrules script type. r=dom-core-reviewers,jjaschke"
    
    This reverts commit 65b30a350ba40728018d7dc14d95906f581cd7a1.
    
    Revert "Bug 2020707 - Part 1: Allow speculation rules to be registered to the document from scripts. r=hsivonen,jjaschke"
    
    This reverts commit 28c7050de96b730c2c22500f25dae923d5b28006.
    
    Revert "Bug 2020704 - Part 2: Add parser for speculation rule sets. r=hsivonen,jjaschke"
    
    This reverts commit eabb8c6eb327e8038d17b5a0d799dbd23f1db382.
    
    Revert "Bug 2020704 - Part 1: Add speculation rules crate. r=dom-core-reviewers,jjaschke"
    
    This reverts commit cd65bed005c5c751177b462800750355e05638c1.

--- dom/script/ScriptElement.cpp
+++ dom/script/ScriptElement.cpp
@@ -255,9 +255,7 @@ bool ScriptElement::MaybeProcessScript(const nsAString& aSourceText) {
   if (!type.IsEmpty()) {
     if (!nsContentUtils::IsJavascriptMIMEType(type) &&
         !type.LowerCaseEqualsASCII("module") &&
-        !type.LowerCaseEqualsASCII("importmap") &&
-        !(StaticPrefs::dom_speculation_rules_enabled() &&
-          type.LowerCaseEqualsASCII("speculationrules"))) {
+        !type.LowerCaseEqualsASCII("importmap")) {
 #ifdef DEBUG
       // There is a WebGL convention to store strings they need inside script
       // tags with these specific unknown script types, so don't warn for them.
