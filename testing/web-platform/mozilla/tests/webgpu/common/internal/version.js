7bb5b74ddc987b5499b21d768224420788e389cc
Erich Gubler <erichdongubler@gmail.com>
2026-06-19 13:10:48 +0000

    Bug 2048117 - chore(webgpu): update CTS to cf0073691abc3b97587411c105e14f0af795db37 r=webgpu-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D307167

--- testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
+++ testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '9a848e6eae8bc1efda4908fd3f2e194bebf8d2c7';
+export const version = 'cf0073691abc3b97587411c105e14f0af795db37';
