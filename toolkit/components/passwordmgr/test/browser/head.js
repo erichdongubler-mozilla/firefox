7afd72bfd3107fe13d045af70086114fbac8ee22
Leo Tenenbaum <ltenenbaum@mozilla.com>
2026-06-25 16:42:25 +0000

    Bug 1998887 - Do navigation for form submission asynchronously. r=farre,credential-management-reviewers,dimi,necko-reviewers,kershaw,devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D302828

--- toolkit/components/passwordmgr/test/browser/head.js
+++ toolkit/components/passwordmgr/test/browser/head.js
@@ -188,18 +188,15 @@ async function getFormSubmitResponseResult(
   { username = "#user", password = "#pass" } = {}
 ) {
   // default selectors are for the response page produced by formsubmit.sjs
-  // TODO: Switch to SpecialPowers.spawn
-  // eslint-disable-next-line mozilla/reject-contenttask-spawn
-  let fieldValues = await ContentTask.spawn(
+  if (!new URL(browser.currentURI.spec).pathname.endsWith(resultURL)) {
+    await BrowserTestUtils.browserLoaded(browser, false, url => {
+      return new URL(url).pathname.endsWith(resultURL);
+    });
+  }
+  let fieldValues = await SpecialPowers.spawn(
     browser,
-    { resultURL, usernameSelector: username, passwordSelector: password },
-    async function ({ resultURL, usernameSelector, passwordSelector }) {
-      await ContentTaskUtils.waitForCondition(() => {
-        return (
-          content.location.pathname.endsWith(resultURL) &&
-          content.document.readyState == "complete"
-        );
-      }, `Wait for form submission load (${resultURL})`);
+    [username, password],
+    (usernameSelector, passwordSelector) => {
       let username =
         content.document.querySelector(usernameSelector).textContent;
       // Bug 1686071: Since generated passwords can have special characters in them,
