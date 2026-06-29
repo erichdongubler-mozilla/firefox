ca5d012d24a3d378f59c67f5fcf7c49dedf5a2f8
Harshit Sohaney <hsohaney@mozilla.com>
2026-06-29 19:47:39 +0000

    Bug 2033325 - (part 1) Probe backup directory access to trigger permission prompts at appropriate instances. r=fluent-reviewers,omc-reviewers,aborondo,bolsson,iholmes,emcminn
    When the user clicks confirm in the turn-on-scheduled-backups dialog,
    probe read access to the selected backup directory. If access is denied,
    clear the location and show an error. Also add PrepareRestoreDialog to
    wait for findBackupsInWellKnownLocations before opening the restore dialog.
    
    Differential Revision: https://phabricator.services.mozilla.com/D306859

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3518,13 +3518,8 @@ pref("browser.backup.enabled", true);
 pref("browser.backup.scheduled.enabled", false);
 
 // Prefs to control visibility and usability of the create backup and restore from backup features.
-#ifndef XP_MACOSX
-  pref("browser.backup.archive.enabled", true);
-  pref("browser.backup.restore.enabled", true);
-#else
-  pref("browser.backup.archive.enabled", false);
-  pref("browser.backup.restore.enabled", false);
-#endif
+pref("browser.backup.archive.enabled", true);
+pref("browser.backup.restore.enabled", true);
 
 // The number of SQLite database pages to backup per step.
 pref("browser.backup.sqlite.pages_per_step", 50);
