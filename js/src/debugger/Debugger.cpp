0c93c487e706cee8ca9e52e62f29e1cf1b852438
Jon Coppeard <jcoppeard@mozilla.com>
2026-06-29 13:17:41 +0000

    Bug 2043304 - Disallow calling module testing functions from debugger hooks r=spidermonkey-reviewers,dminor
    This disallows certain testing functions from being called from inside any
    debugger hooks. These functions are not normally accessible to user code anyway
    so this is not an issue.
    
    To do this I had to add EnterDebuggeeNoExecute to the debugger OnGarbageCollect
    hook. I presume that strengthening the requirements here is OK. This brings it
    into line with all the other hooks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D308460

--- js/src/debugger/Debugger.cpp
+++ js/src/debugger/Debugger.cpp
@@ -7713,14 +7713,25 @@ JS_PUBLIC_API bool FireOnGarbageCollectionHook(
     }
   }
 
+  // Preserve the debuggee's microtask event queue while we run the hooks, so
+  // the debugger's microtask checkpoints don't run from the debuggee's
+  // microtasks, and vice versa.
+  JS::AutoDebuggerJobQueueInterruption adjqi;
+  if (!adjqi.init(cx)) {
+    cx->clearPendingException();
+    return false;
+  }
+
   for (; !triggered.empty(); triggered.popBack()) {
     Debugger* dbg = Debugger::fromJSObject(triggered.back());
+    EnterDebuggeeNoExecute nx(cx, *dbg, adjqi);
 
     if (dbg->getHook(Debugger::OnGarbageCollection)) {
       (void)dbg->enterDebuggerHook(cx, [&]() -> bool {
         return dbg->fireOnGarbageCollectionHook(cx, data);
       });
       MOZ_ASSERT(!cx->isExceptionPending());
+      adjqi.runJobs();
     }
   }
 
