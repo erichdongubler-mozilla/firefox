d4bea9f805fc73867ff7ee7a344e86308569dc46
Kagami Sascha Rosylight <krosylight@proton.me>
2026-09-03 11:43:52 +0000

    Bug 2065919 - Check shutdown again before async write on read fulfill r=dom-core-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D322987

--- dom/streams/ReadableStreamPipeTo.cpp
+++ dom/streams/ReadableStreamPipeTo.cpp
@@ -619,6 +619,11 @@ void PipeToPump::OnReadFulfilled(JSContext* aCx, JS::Handle<JS::Value> aChunk,
          const RefPtr<WritableStreamDefaultWriter>& aWriter,
          JS::Handle<JS::Value> aChunk)
           MOZ_CAN_RUN_SCRIPT_FOR_DEFINITION -> already_AddRefed<Promise> {
+            // Shutdown may have happened after a microtask
+            if (aSelf->mShuttingDown) {
+              return nullptr;
+            }
+
             RefPtr<Promise> promise =
                 WritableStreamDefaultWriterWrite(aCx, aWriter, aChunk, aRv);
 
